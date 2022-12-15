b13262546f50358c254bd4be6b7c29aa1a9102e3
Felix Jochum <s5991044@stud.uni-frankfurt.de>
2022-12-15 09:43:32 +0000

    Update input/input_clark_2d.f90, input/input_clark_2d_bq.f90, input/input_clark_2d_si.f90, input/input_clark_2d_si_bq.f90, input/input_clark_3d.f90, input/input_clark_3d_bq.f90, input/input_clark_3d_si.f90, input/input_clark_3d_si_bq.f90, input/input_cos.f90, input/input_cos_bq.f90, input/input_cos_si.f90, input/input_cos_si_bq.f90, input/input_efficiency.f90, input/input_efficiency_bq.f90, input/input_efficiency_si_bq.f90, input/input_efficiency_si.f90, input/input_flat.f90, input/input_flat_bq.f90, input/input_flat_si.f90, input/input_flat_si_bq.f90, input/input_klemp_2d.f90, input/input_klemp_2d_bq.f90, input/input_klemp_2d_si.f90, input/input_klemp_3d.f90, input/input_klemp_2d_si_bq.f90, input/input_klemp_3d_bq.f90, input/input_klemp_3d_si.f90, input/input_klemp_3d_si_bq.f90, input/input_lock_2d.f90, input/input_lock_2d_bq.f90, input/input_lock_2d_si.f90, input/input_lock_2d_si_bq.f90, input/input_lock_3d.f90, input/input_lock_3d_bq.f90, input/input_lock_3d_si.f90, input/input_lock_3d_si_bq.f90, input/input_rotated.f90, input/input_rotated_bq.f90, input/input_rotated_si.f90, input/input_rotated_si_bq.f90, input/input_test.f90, input/input_schaer_si.f90, input/input_test_bq.f90, input/input_test_si.f90, input/input_test_si_bq.f90, submit/run_clark_2d.sh, submit/run_clark_2d_bq.sh, submit/run_clark_2d_si.sh, submit/run_clark_2d_si_bq.sh, submit/run_clark_3d.sh, submit/run_clark_3d_bq.sh, submit/run_clark_3d_si.sh, submit/run_clark_3d_si_bq.sh, submit/run_cos.sh, submit/run_cos_bq.sh, submit/run_cos_si.sh, submit/run_cos_si_bq.sh, submit/run_efficiency.sh, submit/run_efficiency_bq.sh, submit/run_flat.sh, submit/run_flat_bq.sh, submit/run_flat_si.sh, submit/run_flat_si_bq.sh, submit/run_klemp_2d.sh, submit/run_klemp_2d_bq.sh, submit/run_klemp_2d_si.sh, submit/run_klemp_2d_si_bq.sh, submit/run_klemp_3d.sh, submit/run_klemp_3d_bq.sh, submit/run_klemp_3d_si.sh, submit/run_klemp_3d_si_bq.sh, submit/run_lock_2d.sh, submit/run_lock_2d_bq.sh, submit/run_lock_2d_si.sh, submit/run_lock_2d_si_bq.sh, submit/run_lock_3d.sh, submit/run_lock_3d_bq.sh, submit/run_lock_3d_si.sh, submit/run_lock_3d_si_bq.sh, submit/run_rotated.sh, submit/run_rotated_bq.sh, submit/run_rotated_si.sh, submit/run_rotated_si_bq.sh, submit/run_schaer_si.sh, submit/run_test_bq.sh, submit/run_test.sh, submit/run_test_si.sh, submit/run_test_si_bq.sh, submit_old/run_hotBubble3D.sh, submit_old/run_wavePacket3D.sh, submit_old/run_BK19_50.sh, submit_old/run_BK19_IGW1_si.sh

---
+++ submit/run_rotated_bq.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --partition=general1
+#SBATCH --job-name=rotated_bq
+##switch off multi-threading
+##BATCH --hint=nomultithread
+#SBATCH --ntasks=400
+##usually nodes determined by machine. However, if more memory is needed ...
+##SBATCH --nodes=2
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2600
+#SBATCH --mail-type=FAIL
+#SBATCH --time=02:00:00
+
+set -x
+
+# no. of processors ntasks must be nprocx * nprocy
+ntasks=400
+nprocx=20
+nprocy=20
+
+# OpenMP settings
+export OMP_NUM_THREADS=1
+
+# gprof for MPI
+export GMON_OUT_PREFIX=gmon.out-
+
+# env variable export
+export LD_LIBRARY_PATH=LD_LIBRARY_PATH:/home/atmodynamics/jochum/spack/opt/spack/linux-scientific7-x86_64/intel-18.0.3/hypre-2.15.1-j7lo2mzfhd7bnrcivaf6bsaqjwimsp3m/lib/
+
+dirHome=/home/atmodynamics/jochum/master
+dirScratch=/scratch/atmodynamics/jochum/master
+
+dirNam=${dirHome}/pincflow_tfc_semi_implicit_boussinesq/input
+exe=${dirHome}/pincflow_tfc_semi_implicit_boussinesq/bin/pinc
+dirWork=${dirScratch}/pincflow_tfc_semi_implicit_boussinesq/rotated_bq
+
+mkdir ${dirWork}
+
+# go to work
+
+cd ${dirWork}
+rm *
+
+# copy namelist
+sed -e "s/{nprocx}/${nprocx}/" \
+    -e "s/{nprocy}/${nprocy}/" \
+        ${dirNam}/input_rotated_bq.f90 > input.f90
+
+# run the raytracer
+mpirun -np ${ntasks} ${exe} 1>run.log 2>&1
+
+exit 0
