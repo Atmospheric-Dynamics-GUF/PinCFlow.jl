19baee8dbcef87a8066e2b2c9b1e8b5b32e4a94b
Felix Jochum <s5991044@stud.uni-frankfurt.de>
2022-12-20 16:45:52 +0000

    Update tests/submit/goethe/run_barLC.sh, tests/submit/goethe/run_coldBubble.sh, tests/submit/goethe/run_hotBubble3D.sh, tests/submit/goethe/run_IGW.sh, tests/submit/goethe/run_mountainwave.sh, tests/submit/goethe/run_mountainwave_msgwam.sh, tests/submit/goethe/run_wavePacket3D.sh Deleted tests/input/input_IGW.f90, tests/input/input_barLC.f90, tests/input/input_coldBubble.f90, tests/input/input_hotBubble3D.f90, tests/input/input_mountainwave.f90, tests/input/input_mountainwave_msgwam.f90, tests/input/input_vertical_periodicity.f90, tests/input/input_wavePacket3D.f90

---
+++ tests/submit/goethe/run_hotBubble3D.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --partition=general2
+#SBATCH --job-name=hotBubble3D
+#SBATCH --ntasks=256
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1000
+#SBATCH --mail-type=FAIL
+#SBATCH --time=00:30:00
+
+set -x
+
+# cpu settings
+ntasks=256
+nprocx=16
+nprocy=16
+
+# OpenMP settings
+export OMP_NUM_THREADS=1
+
+# gprof for MPI
+export GMON_OUT_PREFIX=gmon.out-
+
+# env variable export
+export LD_LIBRARY_PATH=LD_LIBRARY_PATH:/home/atmodynamics/jochum/libraries/spack/opt/spack/linux-scientific7-haswell/gcc-4.8.5/hypre-2.26.0-fgfu4ncdxl7ullqryad7jtdzgzqfacjw/lib/
+
+dirHome=/home/atmodynamics/jochum/dissertation
+dirScratch=/scratch/atmodynamics/jochum/dissertation
+
+dirNam=${dirHome}/pincflow/tests/input
+exe=${dirHome}/pincflow/bin/pinc
+dirWork=${dirScratch}/pincflow/tests/hotBubble3D
+
+mkdir ${dirWork}
+
+# go to work
+
+cd ${dirWork}
+rm *
+
+# copy namelist
+sed -e "s/{nprocx}/${nprocx}/" \
+   -e "s/{nprocy}/${nprocy}/" \
+       ${dirNam}/input_hotBubble3D.f90 > input.f90
+
+# run the raytracer
+mpirun -np ${ntasks} ${exe} 1>run.log 2>&1
+
+exit 0
