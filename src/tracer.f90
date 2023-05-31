e2b9244823969294b22686feb7094379f837888c
Irmgard <irmgard.knop@fau.de>
2023-05-31 11:52:24 +0200

    Alpha change

--- src/tracer.f90
+++ src/tracer.f90
@@ -28,91 +28,7 @@ contains
     integer :: ii , jj , kk
 
     select case (tracerSetup)
-
-
-    case ( "thin_layer" )
-
-       tracer0 = 1.0
-
-       var(:,:,:,iVart) = 0.
-       var(:,:,nz/2,iVart) = tracer0
-       
-
-    case ( "gaussian_tracer_2D" )
-
-       tracer0 = 1.0
-       xt0 = 0.5*nx*dx!0.75*nx*dx
-       zt0 = nz/2.*dz
-       sigtx = 0.1*nx*dx
-       sigtz = 0.05*nz*dz
-
-       do ii = 1,nx
-          do jj = 1,ny
-             do kk = 1,nz
-                if (topography) then
-                   zcart = jac(ii,jj,kk) * z(kk) + topography_surface(ii,jj)
-                else
-                   zcart = z(kk)
-                end if
-                var(ii,jj,kk,iVart) = tracer0* &
-                     & exp(-(x(ii)-xt0)**2./(2.*sigtx**2)) * &
-                     & exp(-(zcart-zt0)**2./(2.*sigtz**2))
-             end do
-          end do
-       end do
-
-    case ( "gaussian_tracer_3D" )
-
-       tracer0 = 1.0
-       xt0 = 0.75*nx*dx
-       yt0 = 0.75*ny*dy
-       zt0 = nz/2.*dz
-       sigtx = 0.1*nx*dx
-       sigty = 0.1*ny*dy
-       sigtz = 0.05*nz*dz
-
-       do ii = 1,nx
-          do jj = 1,ny
-             do kk = 1,nz
-                var(ii,jj,kk,iVart) = tracer0* &
-                     & exp(-(x(ii)-xt0)**2./(2.*sigtx**2)) * &
-                     & exp(-(y(jj)-yt0)**2./(2.*sigty**2)) * &
-                     & exp(-(z(kk)-zt0)**2./(2.*sigtz**2))
-             end do
-          end do
-       end do
-       
-
-    case ( "layer_tracer" )
-
-       tracer0 = 1.0
-       zt0 = nz/2.*dz
-       
-       do kk = 1,nz
-          var(:,:,kk,iVart) = tracer0*exp(-(z(kk)-zt0)**2./(2.*2.**2.))
-       end do
-
-    case ( "sin_cos_tracer" )
-
-       tracer0 = 1.0
-       
-       k0t = pi/(mountainWidth_dim / lRef)
-       m0t = pi/(lz_dim(1) / lRef)
-       
-       do ii = 1,nx
-          do jj = 1,ny
-             do kk = 1,nz
-                if (topography) then
-                   zcart = jac(ii,jj,kk) * z(kk) + topography_surface(ii,jj)
-                else
-                   zcart = z(kk)
-                end if
-                var(ii,jj,kk,iVart) = tracer0/2.* &
-                     &(1+ cos(k0t*x(ii))) * sin(m0t*zcart)**2.
-             end do
-          end do
-       end do
-
+   
     case( "increase_in_z_tracer" )
 
        
@@ -132,23 +48,21 @@ contains
           end do
        end do
 
-       dz_tr = 1.0 *lRef!/ (lz(1) - lz(0))
-
-       print*, "dz_tr = ", dz_tr
+       alpha = 1.0 *lRef!/ (lz(1) - lz(0))
        
        if(topography) then
           do kk = 1, nz
              do jj = 1, ny
                 do ii = 1, nx
                    
-                   var(ii,jj,kk, iVart) = rho(ii,jj,kk) * dz_tr * heightTFC(ii, jj, kk)
+                   var(ii,jj,kk, iVart) = rho(ii,jj,kk) * alpha * heightTFC(ii, jj, kk)
 
                 end do
              end do
           end do
        else
           do kk = 1, nz
-             var(:,:,kk, iVart) = rho(:,:,kk) * dz_tr * (z(kk) -z(1)) 
+             var(:,:,kk, iVart) = rho(:,:,kk) * alpha * (z(kk) -z(1)) 
           end do
        end if
 
@@ -174,9 +88,6 @@ contains
 
        call random_number(var(:,:,:,1))
        var(:,:,:,iVart) = var(:,:,:,1)
-
-       
-
 
     case default
        stop "tracer.f90: unkown tracer distribution setup"
