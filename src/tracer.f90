67030fbfe7ce5171199adc53d74b3856feba9d40
Irmgard <irmgard.knop@fau.de>
2023-06-19 09:52:04 +0200

    Had to redo wavePacket testcase. Let's see...

--- src/tracer.f90
+++ src/tracer.f90
@@ -15,10 +15,9 @@ contains
 
     real, dimension(-nbx:nx+nbx,-nby:ny+nby,-nbz:nz+nbz,nVar), &
          & intent(inout) :: var
-    real, dimension(-nbx:nx+nbx,-nby:ny+nby,-nbz:nz+nbz)::rho
+    real, dimension(-nbx:nx+nbx,-nby:ny+nby,-nbz:nz+nbz)::rho, tracerprime
 
     real :: tracer0 ! initial max. tracer ampl.
-    real :: tracer
     real :: xt0 , yt0 , zt0
     real :: sigtx, sigty, sigtz
     real :: k0t , m0t
@@ -32,6 +31,9 @@ contains
    
     case( "increase_in_z_tracer" )
 
+       if (testCase == 'wavePacket') then
+          tracerprime = var(:, :, :, iVart)
+       end if
        
        do kk = 1,nz
           do jj = 1,ny
@@ -55,10 +57,10 @@ contains
           do kk = 1, nz
              do jj = 1, ny
                 do ii = 1, nx
-                   if (testCase == 'wavePacketTracer') then
-                      var(ii, jj, kk, iVart) = (var(ii, jj, kk, iVart) + alpha * heightTFC(ii, jj, kk))*rho(ii, jj, kk)
+                   if (testCase == 'wavePacket') then
+                      var(ii, jj, kk, iVart) = rho(ii,jj,kk)*(tracerprime(ii, jj, kk) + alpha * heightTFC(ii, jj, kk))
                    else
-                      var(ii,jj,kk, iVart) = rho(ii,jj,kk) * alpha * heightTFC(ii, jj, kk)
+                      var(ii,jj,kk, iVart)   = rho(ii,jj,kk)*alpha * heightTFC(ii, jj, kk)
                    end if
 
                 end do
@@ -66,37 +68,16 @@ contains
           end do
        else
           do kk = 1, nz
-             if (testCase == 'wavePacketTracer') then
-                var(:, :, kk, iVart) = rho(:, :, kk) * (var(:, :, kk, iVart) + alpha * (z(kk)-z(1)))
+             if (testCase == 'wavePacket') then
+                var(:, :, kk, iVart) = rho(:, :, kk) * (tracerprime(:, :, kk) + alpha * (z(kk)-z(1)))
              else
                 var(:,:,kk, iVart) = rho(:,:,kk) * alpha * (z(kk) -z(1))
              end if
           end do
        end if
 
-    case( "like_density")
-
-       do kk = 1,nz
-          do jj = 1,ny
-             do ii = 1,nx
-                if (fluctuationMode) then
-                   if (topography) then
-                      var(ii,jj,kk,iVart) = var(ii,jj,kk,1) + rhoStratTFC(ii,jj,kk)
-                   else
-                      var(ii,jj,kk,iVart) = var(ii,jj,kk,1) + rhoStrat(kk)
-                   end if
-                else
-                   var(ii,jj,kk,iVart) = var(ii,jj,kk,1)
-                end if
-             end do
-          end do
-       end do
-  
-    case ("test_tracer")
-
-       call random_number(var(:,:,:,1))
-       var(:,:,:,iVart) = var(:,:,:,1)
-
+    
+    
     case default
        stop "tracer.f90: unkown tracer distribution setup"
     end select
