e43ccaff94d352c849baefa5fd916b97fa80b8ff
Irmgard Knop <knop@login01.cm.cluster>
2023-08-24 13:18:25 +0200

    wavepacket tracer corrections

---
+++ submit/old_submit_IK/run_wavepacket_LES_3DWP00_cosine.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --partition=general2
+#SBATCH --job-name=3Dlescos
+#SBATCH --ntasks=128
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2800
+#SBATCH --mail-type=ALL
+#SBATCH --mail-use=s9467794@stud.uni-frankfurt.de
+#SBATCH --time=48:00:00
+
+set -x
+
+# no. of processors ntasks must be nprocx * nprocy
+ntasks=128
+nprocx=64
+nprocy=2
+
+# OpenMP settings
+export OMP_NUM_THREADS=1
+
+# gprof for MPI
+export GMON_OUT_PREFIX=gmon.out-
+
+# env variable export
+# export LD_LIBRARY_PATH=LD_LIBRARY_PATH:/home/atmodynamics/jochum/spack/opt/spack/linux-scientific7-x86_64/intel-18.0.3/hypre-2.15.1-j7lo2mzfhd7bnrcivaf6bsaqjwimsp3m/lib/
+
+dirHome=/home/atmodynamics/knop/pinc-flow-tracer
+dirScratch=/scratch/atmodynamics/knop
+
+dirNam=${dirHome}/input
+exe=${dirHome}/bin/pinc
+dirWork=${dirScratch}/output/wavepacket_3DWP00_1TS
+
+mkdir ${dirWork}
+
+# go to work
+
+cd ${dirWork} && rm *
+
+# copy namelist
+sed -e "s/{nprocx}/${nprocx}/" \
+    -e "s/{nprocy}/${nprocy}/" \
+        ${dirNam}/input_wavepacket_3DWP00.f90 > input.f90
+
+# run the raytracer
+mpirun -np ${ntasks} ${exe} 1>run.log 2>&1
+
+exit 0
