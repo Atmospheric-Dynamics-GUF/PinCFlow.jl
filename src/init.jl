be6220650c341d711800359f1f2a8491dffa8b49
b382140 <knop@iau.uni-frankfurt.de>
2025-02-26 21:46:28 +0100

    initializing atmosphere

--- src/init.jl
+++ src/init.jl
@@ -1,19 +1,18 @@
 function initialize_variables!(semi)
 
-    (; cache, grid, refval) = semi
-    (; x, y, z, zTFC, nx, ny, nz, nbx, nby, nbz) = grid 
-    (; var, rhoStrat, pStrat, thetaStrat, bvsStrat) = cache 
-    (; lRef, uRef) = refval
+    (; equations, cache) = semi
+    (; uRef) = equations
+    (; var, rhoStrat, pStrat, thetaStrat, bvsStrat) = cache
 
-    u = @view var.u 
-    v = @view var.v
-    w = @view var.w
-    rho = @view var.rho
-    exner = @view var.exner
-    
-    u0 = 10. # this should be a namelist parameter
-    v0 = 0.
-    w0 = 0.
+    u = var.u
+    v = var.v
+    w = var.w
+    rho = var.rho
+    exner = var.exner
+
+    u0 = 10.0 # this should be a namelist parameter
+    v0 = 0.0
+    w0 = 0.0
 
     u .= u0 / uRef
     v .= v0 / uRef
@@ -31,3 +30,95 @@ function initialize!(semi)
     initialize_variables!(semi)
 
 end
+
+function initialize_values(nx, ny, nz, nbx, nby, nbz, xmin, xmax, ymin, ymax, zmin, zmax)
+
+    # Constants
+    gamma = 1.4
+    gamma_1 = gamma - 1.0
+    kappa = (gamma - 1.0) / gamma
+    kappaInv = 1.0 / kappa
+    gammaInv = 1.0 / gamma
+
+    # Reference quantities
+    Rsp = 287.0
+    g = 9.81
+    rhoRef = 1.184
+    pRef = 101325.0
+    aRef = sqrt(pRef / rhoRef)
+    uRef = aRef
+    lRef = pRef / rhoRef / g
+    tRef = lRef / aRef
+    thetaRef = aRef^2.0 / Rsp
+    Ma = uRef / aRef
+    Fr = uRef / sqrt(g * lRef)
+    kappa = (gamma - 1.0) / gamma
+    sig = Ma^2 / Fr^2
+
+    FRef = rhoRef * uRef^2 / lRef
+
+    press0_dim = 1.0e5
+
+    # isothermal atmosphere
+
+    Temp0_dim = 300.0
+    T0 = Temp0_dim / thetaRef
+    N2 = Ma^2 / Fr^4 * kappa / T0
+    NN = sqrt(N2)
+
+    mu_viscous_dim = 0.0
+    ReInv = mu_viscous_dim / (uRef * lRef)
+
+    # Reynolds number
+    ReInv = 0.0
+    if false # TODO - Figure it out!
+        ReInv = mu_viscous_dim / (uRef * lRef)
+    end
+
+    Re = if ReInv < 1.0e-20
+        1.0e20
+    else
+        1.0 / ReInv
+    end
+
+    stretch_exponent = 1
+    grid = make_grid(nx=nx, ny=ny, nz=nz, nbx=nbx, nby=nby, nbz=nbz, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, zmin=zmin, zmax=zmax, lRef=lRef, stretch_exponent=stretch_exponent)
+
+    equations = (; gamma, gamma_1, kappaInv, gammaInv, Rsp, g, rhoRef, pRef, aRef, uRef, lRef, tRef, thetaRef, Ma, Fr, kappa, sig, press0_dim, Temp0_dim, T0, N2, NN, mu_viscous_dim, ReInv, Re)
+
+    pStrat = OffsetArray(zeros(Float64, nx + 2nbx + 1, ny + 2nby + 1, nz + 4),
+                        -nbx:nx+nbx, -nby:ny+nby, -1:nz+2)
+
+    thetaStrat = OffsetArray(zeros(Float64, nx + 2nbx + 1, ny + 2nby + 1, nz + 4),
+                            -nbx:nx+nbx, -nby:ny+nby, -1:nz+2)
+
+    rhoStrat = OffsetArray(zeros(Float64, nx + 2nbx + 1, ny + 2nby + 1, nz + 4),
+                        -nbx:nx+nbx, -nby:ny+nby, -1:nz+2)
+
+    bvsStrat = OffsetArray(zeros(Float64, nx + 2nbx + 1, ny + 2nby + 1, nz + 4),
+                        -nbx:nx+nbx, -nby:ny+nby, -1:nz+2)
+
+    u = OffsetArray(zeros(Float64, nx + 1 + 2nbx, ny + 1 + 2nby, nz + 1 + 2nbz), -nbx:nx+nbx, -nby:ny+nby, -nbz:nz+nbz)
+    v, w, rho, exner, rhop = (copy(u), copy(u), copy(u), copy(u), copy(u))
+
+    var = (; u, v, w, rho, exner, rhop)
+
+    ndim = 3
+    flux_u = OffsetArray(zeros(Float64, nx + 1 + 2nbx, ny + 1 + 2nby, nz + 1 + 2nbz, ndim), -nbx:nx+nbx, -nby:ny+nby, -nbz:nz+nbz, 1:ndim)
+
+    flux_u = OffsetArray(zeros(Float64, nx + 1 + 2nbx, ny + 1 + 2nby, nz + 1 + 2nbz, ndim), -nbx:nx+nbx, -nby:ny+nby, -nbz:nz+nbz, 1:ndim)
+
+    flux_v, flux_w, flux_rho, flux_exner, flux_rhop = (copy(flux_u), copy(flux_u), copy(flux_u), copy(flux_u), copy(flux_u))
+
+    flux = (; u=flux_u, v=flux_v, w=flux_w, rho=flux_rho, exner=flux_exner, rhop=flux_rhop)
+
+    rhoTilde = OffsetArray(zeros(Float64, nx + 1 + 2nbx, ny + 1 + 2nby, nz + 1 + 2nbz, ndim, 2), -nbx:nx+nbx, -nby:ny+nby, -nbz:nz+nbz, 1:ndim, 1:2)
+
+    uTilde, vTilde, wTilde, rhopTilde = (copy(rhoTilde), copy(rhoTilde), copy(rhoTilde), copy(rhoTilde))
+
+    cache = (; var, flux, uTilde, vTilde, wTilde, rhoTilde, rhopTilde, pStrat, thetaStrat, rhoStrat, 
+        bvsStrat)
+
+    return (; grid, equations, cache)
+end
+
---
+++ test/inittest.jl
@@ -0,0 +1,7 @@
+semi = initialize_values(300, 1, 100, 3, 3, 3, 0, 60000, 0, 40000, 0, 20000)
+
+initialize_atmosphere!(semi)
+
+initialize_variables!(semi);
+
+
