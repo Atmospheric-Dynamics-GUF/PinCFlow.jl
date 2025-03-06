77c3b476277d1353136e281caa8806ba68eb99de
Arpit Babbar <arpitbabbar@gmail.com>
2025-03-06 20:34:43 +0100

    Add more tests for Poisson solver (#22)

--- test/test_elixirs.jl
+++ test/test_elixirs.jl
@@ -3,20 +3,6 @@ using TrixiBase
 using PinCFlow_dev
 using Test
 
-# used to generate the testing data for easy copy paste
-function get_norms(arr)
-    l1_norm = norm(arr, 1)
-    l2_norm = norm(arr, 2)
-    linf_norm = norm(arr, Inf)
-    return l1_norm, l2_norm, linf_norm
-end
-
-function test_arr(arr, l1_norm, l2_norm, linf_norm; atol = 1e-14, rtol = 1e-14)
-    @test isapprox(norm(arr, 1), l1_norm; atol = atol, rtol = rtol)
-    @test isapprox(norm(arr, 2), l2_norm; atol = atol, rtol = rtol)
-    @test isapprox(norm(arr, Inf), linf_norm; atol = atol, rtol = rtol)
-end
-
 @testset "elixir_mountainwave" begin
     trixi_include(joinpath(pincflow_examples_dir(), "elixir_mountainwave.jl"),
                   nx = 9, nz = 3)
@@ -30,5 +16,5 @@ end
     test_arr(semi.cache.var.rhop, 0.2626779252704753, 0.015356203279767772,
              0.0019438768803848995)
     test_arr(semi.cache.var.exner, 0.004415532955941557, 0.0003705079602120867,
-             4.86721346318564e-5) # Only passes on github CI. Lower tolerance to 9e-7 to pass locally
+             4.86721346318564e-5, tol = 9e-7) # Tolerance 9e-7 is needed to pass tests locally
 end
--- test/test_poisson.jl
+++ test/test_poisson.jl
@@ -98,7 +98,7 @@ end
     test_file(ref_filename, v_pc, tol_l1 = 1e-16, tol_linf = 1e-16)
 end
 
-@testset "calc_RHS" begin
+@testset "calc_RHS and poissonSolver" begin
     semi = initialize_values(300, 1, 100, 3, 3, 3, 0, 60000, 0, 40000, 0, 20000)
     initialize_atmosphere!(semi)
     initialize_variables!(semi)
@@ -121,20 +121,75 @@ end
 end
 
 @testset "Poisson solver" begin
-    semi = initialize_values(300, 1, 100, 3, 3, 3, 0, 60000, 0, 40000, 0, 20000)
-
-    (; equations, cache, grid, met) = semi
-    (; nx, ny, nz, dx, dy, dz) = grid
-    (; pStrat, rhoStrat, jac, var, kr_sp_tfc, kr_sp_w_tfc, bvsStrat) = cache
-
-    # Set "random values"
+    semi = initialize_values(30, 1, 10, 3, 3, 3, 0, 60000, 0, 40000, 0, 20000)
+    initialize_atmosphere!(semi)
+    initialize_variables!(semi)
+    (; cache, grid) = semi
+    rhs = cache.rhs_bicg
+    (; pStrat, rhoStrat, var, kr_sp_tfc, kr_sp_w_tfc) = cache
+    (; topography_surface) = grid
 
     normalizer = 0.01 # To get smaller values where arithmetic is more accurate
-
     set_lin_array_normalizer!(arr) = set_lin_array!(arr, normalizer = normalizer)
+    set_lin_array_normalizer!.((pStrat, rhoStrat, topography_surface))
 
-    set_lin_array_normalizer!.((pStrat, rhoStrat, kr_sp_tfc, kr_sp_w_tfc, bvsStrat,
-                                cache.var.rho))
+    set_lin_array_normalizer!(var.u)
+    set_lin_array_normalizer!(var.v)
+    set_lin_array_normalizer!(var.w)
 
-    set_lin_array_normalizer!.((grid.topography_surface, grid.zTildeS, grid.zS))
+    PinCFlow_dev.calc_RHS(rhs, semi, 1.0)
+    dt = 0.3
+    errFlagBicg = false
+    nIter = 0
+    facprs = 1.0
+    facray = 1.0
+    PinCFlow_dev.poissonSolver(rhs, semi, dt, errFlagBicg, nIter, "expl", facray, facprs)
+
+    test_arr(cache.dp, 6954.114011644017, 406.0728580408809, 32.1315910002854, tol = 1e-9)
+
+    PinCFlow_dev.poissonSolver(rhs, semi, dt, errFlagBicg, nIter, "impl", facray, facprs)
+
+    test_arr(cache.dp, 6949.851010678487, 405.8250898219831, 32.1214829208721, tol = 1e-9)
+
+    PinCFlow_dev.pressureBoundaryCondition(semi)
+
+    test_arr(cache.dp, 26794.367327608583, 799.5740338430224, 32.1214829208721, tol = 1e-9)
+
+    PinCFlow_dev.correctorStep(semi, dt, "expl", facray, facprs)
+
+    test_arr(var.exner, 26794.367327608583, 799.5740338430224, 32.1214829208721, tol = 1e-9)
+    test_arr(var.u, 2996.1961128135836, 250.73352464698718, 64.47576033368075, tol = 1e-9)
+
+    PinCFlow_dev.correctorStep(semi, dt, "impl", facray, facprs)
+
+    set_lin_array_normalizer!.((kr_sp_tfc, kr_sp_w_tfc))
+    @show get_norms(kr_sp_tfc)
+    @show get_norms(kr_sp_w_tfc)
+    test_arr(var.exner, 53588.734655217166, 1599.148067686045, 64.2429658417442, tol = 1e-9)
+    test_arr(kr_sp_tfc, 368.64, 11.368166079012056, 0.54, tol = 1e-9)
+    test_arr(kr_sp_w_tfc, 368.64, 11.368166079012056, 0.54, tol = 1e-9)
+    test_arr(var.u, 1534.76, 23.24188460516906, 0.6, tol = 1e-9)
+    test_arr(cache.corX, 1643.1243369208187, 248.88860795404267, 64.05576033368075,
+             tol = 1e-9)
+    test_arr(cache.corY, 0.15327530873362505, 0.013421375877617665, 0.0033422640397776215,
+             tol = 1e-9)
+    test_arr(var.exner, 53588.734655217166, 1599.148067686045, 64.2429658417442, tol = 1e-9)
+
+    PinCFlow_dev.bicgstab(rhs, dt, semi, cache.sol_bicg, nIter, errFlagBicg, "expl")
+
+    @show get_norms(cache.sol_bicg)
+    @show get_norms(cache.p_bicg)
+    test_arr(cache.sol_bicg, 1142.0651583477945, 66.0122418332421, 4.178712816389702,
+             tol = 1e-9)
+    test_arr(cache.p_bicg, 5.676571678854043e-6, 3.743420434190437e-7,
+             3.6087341096433547e-8,
+             tol = 1e-9)
+
+    PinCFlow_dev.bicgstab(rhs, dt, semi, cache.sol_bicg, nIter, errFlagBicg, "impl")
+
+    test_arr(cache.sol_bicg, 1142.0651583477945, 66.0122418332421, 4.178712816389702,
+             tol = 1e-9)
+    test_arr(cache.p_bicg, 5.676571678854043e-6, 3.743420434190437e-7,
+             3.6087341096433547e-8,
+             tol = 1e-9)
 end
