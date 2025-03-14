567a759b16ec8c4810e26df6d9f9c45fe700a6e8
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2025-03-14 16:52:59 +0100

    Added halo functions and global dot product.

---
+++ src/mpi/global_operations.jl
@@ -0,0 +1,38 @@
+function compute_global_dot_product(
+  a::OffsetArray{AbstractFloat, 3},
+  b::OffsetArray{AbstractFloat, 3},
+  domain::Domain,
+)
+
+  # Get parameters.
+  (; comm, root) = domain
+
+  # Get shapes.
+  asize = size(a)
+  bsize = size(b)
+
+  # Check if shapes agree.
+  for i in 1:3
+    if asize[i] != bsize[i]
+      println("Error in global_dot_product: Shapes disagree!")
+      exit()
+    end
+  end
+
+  # Compute local dot product.
+  local_dot_product = 0.0
+  for k in 1:asize[3]
+    for j in 1:asize[2]
+      local_dot_product = local_dot_product + dot(a[:, j, k], b[:, j, k])
+    end
+  end
+
+  # Sum over all processes.
+  global_dot_product = MPI.Reduce(local_dot_product, sum, comm; root = root)
+
+  # Broadcast the result.
+  MPI.bcast(global_dot_product, comm; root = root)
+
+  # Return the result.
+  return global_dot_product
+end
---
+++ src/mpi/meridional_halos.jl
@@ -0,0 +1,153 @@
+function set_meridional_halos_of_field!(
+  field::OffsetMatrix{<:AbstractFloat},
+  namelists::Namelists,
+  domain::Domain,
+)
+
+  # Get all necessary fields.
+  (; nbx, nby) = namelists.domain
+  (; comm, nx, ny) = domain
+
+  # Find the neighbour processors and initialize send and reveive buffers.
+  (back, forw) = MPI.Cart_shift(comm, 1, 1)
+  (ysliceback_send, ysliceforw_send, ysliceback_recv, ysliceforw_recv) = (
+    OffsetArray(zeros((nx + 2 * nbx + 1, nby)), (-nbx):(nx + nbx), nby) for
+    i in 1:4
+  )
+
+  # Set slice size.
+  sendcount = nby * (nx + 2 * nbx + 1)
+  recvcount = sendcount
+
+  # Read slice into contiguous array.
+  for j in 1:nby
+    ysliceback_send[:, j] = field[:, j]
+    ysliceforw_send[:, j] = field[:, ny - nby + j]
+  end
+
+  # back -> forw
+  source = back
+  dest = forw
+  tag = 100
+
+  MPI.Sendrecv!(ysliceforw_send, ysliceback_recv, comm, dest, tag, source)
+
+  # forw -> back
+  source = forw
+  dest = back
+  tag = 100
+
+  Mpi.Sendrecv!(ysliceback_send, ysliceforw_recv, comm, dest, tag, source)
+
+  # write auxiliary slice to var field
+  for j in 1:nby
+    field[:, ny + j] = ysliceforw_recv[:, j]
+    field[:, -nby + j] = ysliceback_recv[:, j]
+  end
+
+  return
+end
+
+function set_meridional_halos_of_field!(
+  field::OffsetArray{<:AbstractFloat, 3},
+  namelists::Namelists,
+  domain::Domain,
+)
+
+  # Get all necessary fields.
+  (; nbx, nby, nbz) = namelists.domain
+  (; comm, nx, ny, nz) = domain
+
+  # Find the neighbour processors and initialize send and reveive buffers.
+  (back, forw) = MPI.Cart_shift(comm, 1, 1)
+  (ysliceback_send, ysliceforw_send, ysliceback_recv, ysliceforw_recv) = (
+    OffsetArray(
+      zeros((nx + 2 * nbx + 1, nby, nz + 2 * nbz + 1)),
+      (-nbx):(nx + nbx),
+      nby,
+      (-nbz):(nz + nbz),
+    ) for i in 1:4
+  )
+
+  # Set slice size.
+  sendcount = nby * (nx + 2 * nbx + 1) * (nz + 2 * nbz + 1)
+  recvcount = sendcount
+
+  # Read slice into contiguous array.
+  for j in 1:nby
+    ysliceback_send[:, j, :] = field[:, j, :]
+    ysliceforw_send[:, j, :] = field[:, ny - nby + j, :]
+  end
+
+  # back -> forw
+  source = back
+  dest = forw
+  tag = 100
+
+  MPI.Sendrecv!(ysliceforw_send, ysliceback_recv, comm, dest, tag, source)
+
+  # forw -> back
+  source = forw
+  dest = back
+  tag = 100
+
+  Mpi.Sendrecv!(ysliceback_send, ysliceforw_recv, comm, dest, tag, source)
+
+  # write auxiliary slice to var field
+  for j in 1:nby
+    field[:, ny + j, :] = ysliceforw_recv[:, j, :]
+    field[:, -nby + j, :] = ysliceback_recv[:, j, :]
+  end
+
+  return
+end
+
+function set_meridional_halos_of_field!(
+  field::OffsetArray{<:AbstractFloat, 5},
+  namelists::Namelists,
+  domain::Domain,
+)
+  (back, forw) = MPI.Cart_shift(comm, 1, 1)
+  (ysliceback_send, ysliceforw_send, ysliceback_recv, ysliceforw_recv) = (
+    OffsetArray(
+      zeros((nx + 2 * nbx + 1, nby, nz + 2 * nbz + 1, 3, 2)),
+      (-nbx):(nx + nbx),
+      nby,
+      (-nbz):(nz + nbz),
+      1:3,
+      0:1,
+    ) for i in 1:4
+  )
+
+  # Set slice size.
+  sendcount = nby * (nx + 2 * nbx + 1) * (nz + 2 * nbz + 1) * 6
+  recvcount = sendcount
+
+  # Read slice into contiguous array.
+  for j in 1:nby
+    ysliceback_send[:, j, :, :, :] = field[:, j, :, :, :]
+    ysliceforw_send[:, j, :, :, :] = field[:, ny - nby + j, :, :, :]
+  end
+
+  # back -> forw
+  source = back
+  dest = forw
+  tag = 100
+
+  MPI.Sendrecv!(ysliceforw_send, ysliceback_recv, comm, dest, tag, source)
+
+  # forw -> back
+  source = forw
+  dest = back
+  tag = 100
+
+  Mpi.Sendrecv!(ysliceback_send, ysliceforw_recv, comm, dest, tag, source)
+
+  # write auxiliary slice to var field
+  for j in 1:nby
+    field[:, ny + j, :, :, :] = ysliceforw_recv[:, j, :, :, :]
+    field[:, -nby + j, :, :, :] = ysliceback_recv[:, j, :, :, :]
+  end
+
+  return
+end
