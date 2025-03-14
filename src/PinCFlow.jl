fa29c7870a8053cef590e250dba4f1115b6ab770
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2025-03-14 18:06:50 +0100

    Updated PinCFlow.jl.

--- src/PinCFlow.jl
+++ src/PinCFlow.jl
@@ -7,11 +7,11 @@ using MPI
 
 using SimpleUnPack
 using TimerOutputs
-using TrixiBase # get the `timer` object
+using TrixiBase
 
 # Namelists
 include("namelists/atmosphere_namelist.jl")
-include("namelists/boundary_namelist.jl")
+include("namelists/boundaries_namelist.jl")
 include("namelists/discretization_namelist.jl")
 include("namelists/domain_namelist.jl")
 include("namelists/grid_namelist.jl")
@@ -20,7 +20,7 @@ include("namelists/output_namelist.jl")
 include("namelists/poisson_namelist.jl")
 include("namelists/settings_namelist.jl")
 include("namelists/sponge_namelist.jl")
-#
+
 # Atmosphere
 include("atmosphere/atmosphere.jl")
 include("atmosphere/constants.jl")
@@ -28,20 +28,28 @@ include("atmosphere/grid.jl")
 include("atmosphere/sponge.jl")
 
 # Boundaries
-include("boundaries/boundary.jl")
-include("boundaries/domain.jl")
+include("boundaries/boundaries.jl")
+include("boundaries/meridional_boundaries.jl")
+include("boundaries/vertical_boundaries.jl")
+include("boundaries/zonal_boundaries.jl")
 
 # Fluxes
-include("fluxes/calc_flux.jl")
+include("fluxes/mass_fluxes.jl")
+include("fluxes/momentum_fluxes.jl")
 include("fluxes/reconstruction.jl")
 
-# IO
-include("io/save_solution.jl")
+# Integration
+include("integration/integration.jl")
+include("integration/state.jl")
+
+# Output
+include("output/output.jl")
 
 # Poisson
 include("poisson/poisson.jl")
 
 # Update
+include("update/sponge.jl")
 include("update/time.jl")
 include("update/time_loop.jl")
 include("update/time_step.jl")
@@ -52,22 +60,12 @@ include("variables/stress_tensor.jl")
 include("variables/variables.jl")
 include("variables/vertical_wind.jl")
 
-# Model
-include("model.jl")
-
 pincflow_test_dir() = joinpath(dirname(pathof(PinCFlow)), "..", "test")
 pincflow_examples_dir() = joinpath(dirname(pathof(PinCFlow)), "..", "examples")
 
-
 export pincflow, Corrector
-
-
 export pincflow_test_dir, pincflow_examples_dir
-
-
 export Grid, Constants, Atmosphere, Domain, Variables, Fluxes, State
-
-# debugging
 export Corrector
 
 end
