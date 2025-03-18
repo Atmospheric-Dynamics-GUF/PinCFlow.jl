eb2e609e33f319cb790408046cfe22b4362d47d0
Jonas Rothermel <j.rothermel@sinabis-analytics.com>
2025-03-18 14:53:33 +0100

    add tests for boundary

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,13 +8,14 @@ using Trixi: CompressibleEulerEquations1D
 using LinearAlgebra
 using GZip
 
+include("util.jl")
 # include("test_poisson.jl")
 # include("test_elixirs.jl")
 # include("test_fluxes.jl")
 # include("test_update.jl")
 include("test_atmosphere.jl")
 # include("test_sponge.jl")
-# include("test_boundary.jl")
+include("test_boundary.jl")
 
 # include("test_namelist.jl")
 include("test_init.jl")
