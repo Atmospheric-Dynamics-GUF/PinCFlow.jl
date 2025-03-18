298e23bd84ec4323ce151086e416e0cc268722fc
Jonas Rothermel <j.rothermel@sinabis-analytics.com>
2025-03-18 17:22:13 +0100

    test_atmosphere restructure

--- test/test_atmosphere.jl
+++ test/test_atmosphere.jl
@@ -1,5 +1,4 @@
-using Setfield
-@testset "Atmosphere" begin
+@testset "Initialization" begin
   domain = PinCFlow.DomainNamelist(;
     sizex = 3,
     sizey = 1,
@@ -8,21 +7,32 @@ using Setfield
     ly_dim = [0.0, 40000.0],
     lz_dim = [0.0, 20000.0],
   )
-  namelists = Namelists(; domain = domain)
+  grid = PinCFlow.GridNamelist(;
+    mountain_case = 3,
+    mountainheight_dim = 400.0,
+    mountainwidth_dim = 1000.0,
+  )
+  namelists = Namelists(; domain = domain, grid = grid)
   state = PinCFlow.State(namelists)
-  # semi = initialize_values(3, 1, 1, 3, 3, 3, 0, 60000, 0, 40000, 0, 20000)
-  @test state.domain.nx == 3
-  @test state.domain.nz == 1
-  @test state.domain.ny == 1
-  @test state.grid.lx[0] ≈ 0.0
-  @test state.grid.lx[1] ≈ 6.877891931902294
-  @test state.grid.ly[0] ≈ 0.0
-  @test state.grid.ly[1] ≈ 4.585261287934863
-  @test state.grid.lz[0] ≈ 0.0
-  @test state.grid.lz[1] ≈ 2.2926306439674313
-  @test state.grid.dx ≈ 2.2926306439674313
-  @test state.grid.dy ≈ 4.585261287934863
-  @test state.grid.dz ≈ 2.2926306439674313
+  @testset "Domain" begin
+    @test state.domain.nx == 3
+    @test state.domain.nz == 1
+    @test state.domain.ny == 1
+    @test state.domain.nxx == 10
+    @test state.domain.nyy == 8
+    @test state.domain.nzz == 8
+  end
+  @testset "Grid" begin
+    @test state.grid.lx[0] ≈ 0.0
+    @test state.grid.lx[1] ≈ 6.877891931902294
+    @test state.grid.ly[0] ≈ 0.0
+    @test state.grid.ly[1] ≈ 4.585261287934863
+    @test state.grid.lz[0] ≈ 0.0
+    @test state.grid.lz[1] ≈ 2.2926306439674313
+    @test state.grid.dx ≈ 2.2926306439674313
+    @test state.grid.dy ≈ 4.585261287934863
+    @test state.grid.dz ≈ 2.2926306439674313
+  end
 
   tol = 1e-14
   @testset "Constants" begin
@@ -100,12 +110,11 @@ using Setfield
   end
 
   @testset "Jacobibian" begin
-    # TODO - Improve these tests when jac, met are arrays
-    @test_broken state.grid.jac[2, 2, 2] ≈ 0.98
+    @test state.grid.jac[2, 2, 2] ≈ 0.98
   end
   @testset "Metric tensor" begin
-    @test_broken state.grid.met[1, 1, 1, 1, 3] ≈ -0.004987779939149084
+    @test state.grid.met[1, 1, 1, 1, 3] ≈ -0.004987779939149084
     @test state.grid.met[3, 1, 1, 2, 3] ≈ 0.0
-    @test_broken state.grid.met[1, 1, 1, 3, 3] ≈ 1.0001246360352993
+    @test state.grid.met[1, 1, 1, 3, 3] ≈ 1.0001246360352993
   end
 end
