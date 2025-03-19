dc4035ee746840c5a6844ad4f52b17f0ff60e781
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2025-03-19 16:49:01 +0100

    Another bug fix in Preconditioner type.

--- src/types/poisson/preconditioner.jl
+++ src/types/poisson/preconditioner.jl
@@ -1,7 +1,7 @@
-struct Preconditioner{A <: AbstractArray{<:AbstractFloat, 3}}
+struct Preconditioner{A <: AbstractArray{<:AbstractFloat, 3}, B <: AbstractMatrix{<:AbstractFloat}}
   s_pc::A
   q_pc::A
-  p_pc::A
+  p_pc::B
 end
 
 function Preconditioner(domain)
@@ -10,7 +10,8 @@ function Preconditioner(domain)
   (; nx, ny, nz) = domain
 
   # Initialize the preconditioner fields.
-  (s_pc, q_pc, p_pc) = (zeros((nx, ny, nz)) for i in 1:3)
+  (s_pc, q_pc) = (zeros((nx, ny, nz)) for i in 1:2)
+  p_pc = zeros(nx, ny)
 
   # Return a Preconditioner instance.
   return Preconditioner(s_pc, q_pc, p_pc)
