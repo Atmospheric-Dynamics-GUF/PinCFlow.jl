eca2ef255a2c92187ef4a41d08f16d7392edb9b3
Jonas Rothermel <j.rothermel@sinabis-analytics.com>
2025-03-20 15:35:26 +0100

    use views for better performance

--- src/boundaries/set_zonal_boundaries_of_field.jl
+++ src/boundaries/set_zonal_boundaries_of_field.jl
@@ -10,8 +10,8 @@ function set_zonal_boundaries_of_field!(
     set_zonal_halos_of_field!(field, namelists, domain)
   else
     for i in 1:nbx
-      field[nx + i, :] = field[i, :]
-      field[-i + 1, :] = field[nx - i + 1, :]
+      field[nx + i, :] .= view(field, i, :)
+      field[-i + 1, :] .= view(field, nx - i + 1, :)
     end
   end
 
@@ -30,8 +30,8 @@ function set_zonal_boundaries_of_field!(
     set_zonal_halos_of_field!(field, namelists, domain)
   else
     for i in 1:nbx
-      field[nx + i, :, :] = field[i, :, :]
-      field[-i + 1, :, :] = field[nx - i + 1, :, :]
+      field[nx + i, :, :] .= view(field, i, :, :)
+      field[-i + 1, :, :] .= view(field, nx - i + 1, :, :)
     end
   end
 
@@ -50,8 +50,8 @@ function set_zonal_boundaries_of_field!(
     set_zonal_halos_of_field!(field, namelists, domain)
   else
     for i in 1:nbx
-      field[nx + i, :, :, :, :] = field[i, :, :, :, :]
-      field[-i + 1, :, :, :, :] = field[nx - i + 1, :, :, :, :]
+      field[nx + i, :, :, :, :] .= view(field, i, :, :, :, :)
+      field[-i + 1, :, :, :, :] .= view(field, nx - i + 1, :, :, :, :)
     end
   end
 
