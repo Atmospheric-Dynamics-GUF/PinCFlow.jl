5ce5c7844937b057dc1319027986dadf00688517
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2025-03-26 11:39:46 +0100

    Added a function for the synchronization of density fluctuations.

---
+++ src/integration/synchronize_density_fluctuations.jl
@@ -0,0 +1,16 @@
+function synchronize_density_fluctuations!(state::State)
+  (; model) = state.namelists.setting
+  synchronize_density_fluctuations!(state, model)
+end
+
+function synchronize_density_fluctuations!(state::State, model::PseudoIncompressible)
+
+  # Get all necessary fields.
+  (; rho, rhop) = state.variables.predictands
+
+  # Synchronize density fluctuations.
+  rhop .= rho
+
+  # Return.
+  return
+end
