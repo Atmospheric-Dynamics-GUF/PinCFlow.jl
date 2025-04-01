d8c7cc3ff71fbd5162df9fbd7f2b144981132753
Jonas Rothermel <j.rothermel@sinabis-analytics.com>
2025-04-01 18:40:26 +0200

    add first rough version of split_rayvol, not tested

---
+++ src/wkb/split_rayvol.jl
@@ -0,0 +1,184 @@
+function split_z(ix, jy, kz, rays, nray, dz)
+  nrlc = nray[ix, jy, kz]
+  for iray in 1:nray[ix, jy, kz]
+    ray = rays[iray, ix, jy, kz]
+    zr = ray(iRay, ix, jy, kz) % z
+    zr = ray.z
+
+    dzr = ray.dzray
+    azm = ray.area_zm
+
+    xr = ray.x
+    yr = ray.y
+
+    ixrv = nint((xr - lx[0]) / dx + 0.5) - ix0
+    jyrv = nint((yr - ly[0]) / dy + 0.5) - jy0
+    kzrvd = kzTildeTFC[ixrv, jyrv, zr - 0.5 * dzr]
+    kzrvu = kzTildeTFC[ixrv, jyrv, zr + 0.5 * dzr]
+
+    dzmin = dz
+    for kzrv in kzrvd:kzrvu
+      dzmin = min(dzmin, jac[ixrv, jyrv, kzrv] * dz)
+    end
+
+    if (dzr > dzmin)
+      factor = ceil(dzr / dzmin)
+      ray.z = zr + 0.5 * (1 / factor - 1) * dzr
+      ray.dzray = dzr / factor
+      ray.area_zm = azm / factor
+      for jRay in (nrlc + 1):(nrlc + factor - 1)
+        ray[jRay, ix, jy, kz] = ray[iRay, ix, jy, kz]
+        ray[jRay, ix, jy, kz].z =
+          ray[jRay, ix, jy, kz].z + (jRay - nrlc) * dzr / factor
+      end
+      nrlc = nrlc + factor - 1
+    end
+  end
+
+  if (nrlc > nray[ix, jy, kz])
+    nray[ix, jy, kz] = nrlc
+
+    if (nray[ix, jy, kz] > nray_wrk)
+      # print *, 'ERROR at ix,jy,kz =', ix, jy, kz
+      # print *, 'nRay =', nRay(ix, jy, kz), '> nray_wrk =', nray_wrk
+      exit()
+    end
+  end
+end
+
+function split_y(ix, jy, kz, rays, nray, dy)
+  nrlc = nray[ix, jy, kz]
+  for iray in 1:nray(ix, jy, kz)
+    ray = rays(iray, ix, jy, kz)
+    yr = ray.y
+    dyr = ray.dyray
+
+    if (dyr > dy)
+      nrlc = nrlc + 1
+
+      if (nrlc > nray_wrk)
+        # print *, 'r.v. getting too many by splitting in y  direction'
+        exit()
+      end
+
+      ayl = ray.area_yl
+
+      ray.dyray = 0.5 * dyr
+      ray.area_yl = 0.5 * ayl
+
+      # copy ?
+      rays[nrlc, ix, jy, kz] = ray
+
+      ray.y = yr - 0.25 * dyr
+      ray[nrlc, ix, jy, kz].y = yr + 0.25 * dyr
+    end
+  end
+
+  if (nrlc > nray[ix, jy, kz])
+    function nray(ix, jy, kz)
+      return nrlc
+    end
+    if (nray[ix, jy, kz] > nray_wrk)
+      # print *, 'ERROR at ix,jy,kz =', ix, jy, kz
+      # print *, 'nRay =', nRay(ix, jy, kz), '> nray_wrk =', nray_wrk
+      exit()
+    end
+  end
+end
+
+function split_x(ix, jy, kz, rays, nray, dx)
+  nrlc = nray[ix, jy, kz]
+  for iRay in 1:nray[ix, jy, kz]
+    ray = rays[iray, ix, jy, kz]
+    dxr = ray.dxray
+
+    if (dxr > dx)
+      nrlc = nrlc + 1
+
+      if (nrlc > nray_wrk)
+        # print *, 'r.v. getting too many by splitting in x  direction'
+        exit()
+      end
+
+      xr = ray.x
+
+      axk = ray.area_xk
+
+      ray.dxray = 0.5 * dxr
+
+      ray.area_xk = 0.5 * axk
+
+      rays[nrlc, ix, jy, kz] = ray
+
+      ray.x = xr - 0.25 * dxr
+      rays[nrlc, ix, jy, kz].x = xr + 0.25 * dxr
+    end
+  end
+  if (nrlc > nray[ix, jy, kz])
+    nray[ix, jy, kz] = nrlc
+
+    if (nRay[ix, jy, kz] > nray_wrk)
+      # print *, 'ERROR at ix,jy,kz =', ix, jy, kz
+      # print *, 'nRay =', nRay(ix, jy, kz), '> nray_wrk =', nray_wrk
+      exit()
+    end
+  end
+end
+
+function split_rayvol(state)
+  if (state.steady_state)
+    return
+  end
+
+  ix0 = is + nbx - 1
+  jy0 = js + nby - 1
+
+  # ! total number of ray volumes before splitting
+
+  # nrvloc = sum(nRay(1:nx, 1:ny, 1:nz))
+
+  # ! testb
+  # ! print*,'before splitting nrvloc =',nrvloc
+  # ! teste
+
+  # call mpi_reduce(nrvloc, nrvtt0, 1, mpi_integer, mpi_sum, root, comm, ierror)
+  # call mpi_bcast(nrvtt0, 1, mpi_integer, root, comm, ierror)
+  (; nx, ny, nz) = state.domain
+  domain = state.domain
+  (; dx, dy, dz) = state.grid
+  (; rays, nray) = state.wkb
+  for kz in 1:nz
+    for jy in 1:ny
+      for ix in 1:nx
+        if (nray[ix, jy, kz] < 1)
+          continue
+        end
+
+        if (domain.sizex > 1)
+          split_x(ix, jy, kz, rays, nray, dx)
+        end
+        if (domain.sizey > 1)
+          split_y(ix, jy, kz, rays, nray, dy)
+        end
+        split_z(ix, jy, kz, rays, nray, dz)
+      end
+    end
+  end
+
+  # ! total number of ray volumes after splitting
+
+  # nrvloc = sum(nRay(1:nx, 1:ny, 1:nz))
+
+  # ! testb
+  # ! print*,'after splitting nrvloc =',nrvloc
+  # ! teste
+
+  # call mpi_reduce(nrvloc, nrvtt1, 1, mpi_integer, mpi_sum, root, comm, ierror)
+  # call mpi_bcast(nrvtt1, 1, mpi_integer, root, comm, ierror)
+
+  # if(master .and. nrvtt1 > nrvtt0) then
+  #   print *, 'after splitting nray =', nrvtt1
+  # end if
+
+  return
+end
