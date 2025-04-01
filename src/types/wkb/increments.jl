4fbf4c60e4928f065fd6d5ae819040c8d09cdf7b
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2025-04-01 18:51:41 +0200

    Added composite type and constructor for WKB (still very much WIP, though).

---
+++ src/types/wkb/increments.jl
@@ -0,0 +1,15 @@
+struct Increments{A <: AbstractArray{<:AbstractFloat, 4}}
+  dxray::A
+  dyray::A
+  dzray::A
+  dkray::A
+  dlray::A
+  dmray::A
+  ddxray::A
+  ddyray::A
+  ddzray::A
+end
+
+function Increments(nray_wrk::Integer, nxx::Integer, nyy::Integer, nzz::Integer)
+  return Increments([zeros(nray_wrk, nxx, nyy, nzz) for i in 1:9]...)
+end
