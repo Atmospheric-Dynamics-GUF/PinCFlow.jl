64c7781d10f3cb720d89820bf6bd8a205a045ea9
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2025-04-02 11:47:33 +0200

    Added kztfc and kztildetfc.

---
+++ src/wkb/kztfc.jl
@@ -0,0 +1,25 @@
+function kztfc(
+  i::Integer,
+  j::Integer,
+  z::AbstractFloat,
+  domain::Domain,
+  grid::Grid,
+)
+
+  # Get all necessary fields.
+  (; nzz) = domain
+  (; ztfc) = grid
+
+  # Compute the vertical index.
+  @views k = argmin(abs.(ztfc[i, j, :] .- z))
+  if ztfc[i, j, k] < z
+    k += 1
+  end
+  if k < 2
+    k = 2
+  end
+  if k > nzz
+    k = nzz
+  end
+  return k
+end
