b169a6a52ae43f216ae6a191ed305a72113e2409
Jonas Rothermel <j.rothermel@sinabis-analytics.com>
2025-04-07 19:24:25 +0200

    refactor merge_rayvol

--- src/wkb/merge_rayvol.jl
+++ src/wkb/merge_rayvol.jl
@@ -1,20 +1,202 @@
-# TODO: well, this needs a better name...
-struct TemporaryRayVolume
-  xn::Float64
-  xx::Float64
-  yn::Float64
-  yx::Float64
-  zn::Float64
-  zx::Float64
-  kn::Float64
-  kx::Float64
-  ln::Float64
-  lx::Float64
-  mn::Float64
-  mx::Float64
-  wa::Float64
+function wavenumbers(rijk::CartesianIndex{4}, rays::Rays)
+  return rays.k[rijk], rays.l[rijk], rays.m[rijk]
 end
 
+function positions(rijk::CartesianIndex{4}, rays::Rays)
+  return rays.x[rijk], rays.y[rijk], rays.z[rijk]
+end
+
+const Bound = SVector{2, Float64}
+
+function update_bound(r, dr, bound)
+  return Bound(bound[1] - dr / 2.0, bound[2] + dr / 2.0)
+end
+
+function _new_bound(r, dr)
+  return Bound(r - dr / 2.0, r + dr / 2.0)
+end
+
+struct BinnedRayVolumeBounds
+  x::Bound
+  y::Bound
+  z::Bound
+  k::Bound
+  l::Bound
+  m::Bound
+  wa::Float64
+
+  function BinnedRayVolumeBounds(x, dx, y, dy, z, dz, k, dk, l, dl, m, dm, wa)
+    bounds = new(
+      _new_bound(x, dx),
+      _new_bound(y, dy),
+      _new_bound(z, dz),
+      _new_bound(k, dk),
+      _new_bound(l, dl),
+      _new_bound(m, dm),
+      wa,
+    )
+    return bounds
+  end
+end
+
+function midpoint(b::Bound)
+  return (b[1] + b[2]) / 2.0
+end
+
+function diff(b::Bound)
+  return b[1] - b[2]
+end
+
+function generate_merged_rvs(nr_merge, ijk, intervals, rays, domain)
+  sizex, sizey, _ = domainsize(domain)
+  nr_merge .= 0
+  # generate merged rvs
+  for iray in 1:nray[ijk]
+    rijk = CartesianIndex(iray, ijk)
+    k, l, m = wavenumbers(rijk, rays)
+    x, y, z = positions(rijk, rays)
+    dx, dy, dz = position_extents(rijk, rays)
+    dk, dl, dm = wavenumber_extents(rijk, rays)
+    axk, ayl, azm = areas(rijk, rays)
+
+    if sizex > 1
+      fcpspx = axk
+      ir_k = index_klm(rijk, k, intervals[1], nxray)
+    else
+      fcpspx = 1.0
+      ir_k = 1
+    end
+    if sizey > 1
+      fcpspx = ayl
+      ir_l = index_klm(rijk, l, intervals[2], nyray)
+    else
+      fcpspy = 1.0
+      ir_l = 1
+    end
+
+    fcpspz = azm
+    ir_m = index_klm(rijk, m, intervals[3], nzray)
+    jray = ray_index(ir_k, ir_l, ir_m, nxray, nyray, nzray, sizex, sizey)
+
+    nr_merge[jray] += 1
+    if nr_merge[jray] == 1
+      if cons_merge == "wa"
+        wa = wdr * fxpspx * fcpspy * fcpspz
+      else
+        wa = wdr * omir * fcpspx * fcpspy * fcpspz
+      end
+
+      nr_merge[jray] =
+        BinnedRayVolumeBounds(x, dx, y, dy, z, dz, k, dk, l, dl, m, dm, wa)
+    else
+      vol = nr_merge[jray]
+      vol.x = update_bound(x, dx, vol.x)
+      vol.y = update_bound(y, dy, vol.y)
+      vol.z = update_bound(z, dz, vol.z)
+      vol.l = update_bound(l, dl, vol.l)
+      vol.k = update_bound(k, dk, vol.k)
+      vol.m = update_bound(m, dm, vol.m)
+      nr_merge[jray] = vol # not sure if this is needed
+      if cons_merge == "wa"
+        vol.wa += wdr * fcpspx * fcpspy * fcpspz
+      elseif cons_merge == "en"
+        vol.wa += wdr * omir * fcpspx * fcpspy * fcpspz
+      end
+    end
+  end
+end
+
+function replace_rayvolumes!(rays, ijk, nr_merge, nray_max)
+  iray = 0
+  for jray in 1:nray_max
+    if nr_merge[jray] < 1
+      continue
+    end
+    iray += 1
+    rijk = CartesianIndex(iray, ijk)
+    tvol = nr_merge[jray]
+    rays.x[rijk] = midpoint(tvol.x)
+    rays.y[rijk] = midpoint(tvol.y)
+    rays.z[rijk] = midpoint(tvol.z)
+
+    rays.k[rijk] = midpoint(tvol.k)
+    rays.l[rijk] = midpoint(tvol.l)
+    rays.m[rijk] = midpoint(tvol.m)
+
+    rays.dxray[rijk] = diff(tvol.x)
+    rays.dyray[rijk] = diff(tvol.y)
+    rays.dzray[rijk] = diff(tvol.z)
+
+    rays.dkray[rijk] = diff(tvol.k)
+    rays.dlray[rijk] = diff(tvol.l)
+    rays.dmray[rijk] = diff(tvol.m)
+
+    rays.area_xk[rijk] = rays.dxray[rijk] * rays.dkray[rijk]
+    rays.area_yl[rijk] = rays.dyray[rijk] * rays.dlray[rijk]
+    rays.area_zm[rijk] = rays.dzray[rijk] * rays.dmray[rijk]
+    omir = intrinsic_frequency(rijk, branchr, rays)
+    rays.omega[rijk] = omir
+    fcpspx = ifelse(sizex > 1, rays.area_xk[rijk], 1.0)
+    fcpspy = ifelse(sizey > 1, rays.area_yl[rijk], 1.0)
+    fcpspz = rays.area_zm[rijk]
+
+    # wave action density
+    # TODO: as functions
+    if cons_merge == "wa"
+      wa = tvol.wa / (fcpspx * fcpspy * fcpspz)
+    else
+      wa = tvol.wa / (omir * fcpspx * fcpspy * fcpspz)
+    end
+    rays.dens[rijk] = wa
+  end
+end
+
+function intrinsic_frequency(rijk, branchr, rays)
+  k, l, m = wavenumbers(rijk, rays)
+  h = sqrt(k^2 + l^2)
+  z = rays.z[rijk]
+  NNr = stratification(z, 1)
+
+  omir = branchr * sqrt(NNr * h^2 + f_cor_nd^2 * m^2) / sqrt(h^2 + m^2)
+  return omir
+end
+
+function ray_index(ir_k, ir_l, ir_m, nxray, nyray, nzray, sizex, sizey)
+  if sizex > 1
+    if sizey > 1
+      jray =
+        (ir_m - 1) * (nyray - 1) * (nxray - 1) + (ir_l - 1) * (nxray - 1) + ir_k
+    else
+      jray = (ir_m - 1) * (nxray - 1) + ir_k
+    end
+  else
+    if (sizey > 1)
+      jray = (ir_m - 1) * (nyray - 1) + ir_l
+    else
+      jray = ir_m
+    end
+  end
+  return jray
+end
+
+function index_klm(rijk, w, interval, n_ray)
+  if w < 0
+    if abs(log(-w / interval.max_n) / interval.mg_n) < 1.0
+      ir = n_ray / 2 - 1
+    else
+      ir = Int64(log(-w / interval.min_n) / interval.mg_n) + 1
+    end
+  elseif w == 0
+    ir = n_ray / 2
+  else
+    if abs(log(w / interval.max_p) / interval.mg_p < 1.0)
+      ir = n_ray - 1
+    else
+      ir = Int64(log(w / interval.min_p) / interval.mg_p) + n_ray / 2 + 1
+    end
+  end
+  return ir
+end
 function compute_intervals(ijk, rays, nray, domain)
   k_min_p = 0.0
   k_max_p = 0.0
@@ -146,7 +328,7 @@ function merge_rayvol!(rays, state::AbstractState)
   @views nray_before = sum(nray[i0:i1, j0:j1, k0:k1])
   nray_before = MPI.Allreduce(nray_before, +, comm)
 
-  nr_merge = Array{TemporaryRayVolume}(undef, nray_max)
+  nr_merge = Array{BinnedRayVolumeBounds}(undef, nray_max)
 
   for kz in k0:k1, jy in j0:j1, ix in i0:i1
     ijk = CartesianIndex(ix, jy, kz)
@@ -154,175 +336,14 @@ function merge_rayvol!(rays, state::AbstractState)
       continue
     end
 
-    interval_k, interval_l, interval_m =
-      compute_intervals(ijk, rays, nray, state.domain)
+    volume_intervals = compute_intervals(ijk, rays, nray, state.domain)
 
-    nr_merge .= 0
-    # generate merged rvs
-    for iray in 1:nray[ijk]
-      rijk = CartesianIndex(iray, ijk)
-      k, l, m = wavenumbers(rijk, rays)
-      x, y, z = positions(rijk, rays)
-      axk, ayl, azm = areas(rijk, rays)
-
-      if sizex > 1
-        fcpspx = axk
-        ir_k = index_klm(rijk, k, interval_k, nxray)
-      else
-        fcpspx = 1.0
-        ir_k = 1
-      end
-      if sizey > 1
-        fcpspx = ayl
-        ir_l = index_klm(rijk, l, interval_l, nyray)
-      else
-        fcpspy = 1.0
-        ir_l = 1
-      end
-
-      fcpspz = azm
-      ir_m = index_klm(rijk, m, interval_m, nzray)
-      jray = ray_index(ir_k, ir_l, ir_m, nxray, nyray, nzray, sizex, sizey)
-
-      nr_merge[jray] += 1
-      if nr_merge[jray] == 1
-        if cons_merge == "wa"
-          wa = wdr * fxpspx * fcpspy * fcpspz
-        else
-          wa = wdr * omir * fcpspx * fcpspy * fcpspz
-        end
-
-        nr_merge[jray] = TemporaryRayVolume(
-          xr - dxr / 2.0,
-          xr + dxr / 2.0,
-          yr - dyr / 2.0,
-          yr + dyr / 2.0,
-          zr - dzr / 2.0,
-          zr + dzr / 2.0,
-          k - dk / 2.0,
-          k + dk / 2.0,
-          l - dl / 2.0,
-          l + dl / 2.0,
-          m - dm / 2.0,
-          m + dm / 2.0,
-          wa,
-        )
-      else
-        vol = nr_merge[jray]
-        vol.xn = min(vol.xn, x - dx / 2.0)
-        vol.xx = min(vol.xx, x + dx / 2.0)
-        vol.yn = min(vol.yn, y - dy / 2.0)
-        vol.yx = min(vol.yx, y + dy / 2.0)
-        vol.zn = min(vol.zn, z - dz / 2.0)
-        vol.zx = min(vol.zx, z + dz / 2.0)
-        vol.kn = min(vol.kn, k - dk / 2.0)
-        vol.kx = min(vol.kx, k + dk / 2.0)
-        vol.ln = min(vol.ln, l - dl / 2.0)
-        vol.lx = min(vol.lx, l + dl / 2.0)
-        vol.mn = min(vol.mn, m - dm / 2.0)
-        vol.mx = min(vol.mx, m + dm / 2.0)
-        if cons_merge == "wa"
-          vol.wa += wdr * fcpspx * fcpspy * fcpspz
-        elseif cons_merge == "en"
-          vol.wa += wdr * omir * fcpspx * fcpspy * fcpspz
-        end
-      end
-    end
-
-    # now replace old ray volumes by merged ones
-    #
-    iray = 0
-    for jray in 1:nray_max
-      if nr_merge[jray] < 1
-        continue
-      end
-      iray += 1
-      rijk = CartesianIndex(iray, ijk)
-      tvol = nr_merge[jray]
-      rays.x[rijk] = (tvol.xn + tvol.xx) / 2.0
-      rays.y[rijk] = (tvol.yn + tvol.yx) / 2.0
-      rays.z[rijk] = (tvol.zn + tvol.zx) / 2.0
-
-      rays.k[rijk] = (tvol.kx + tvol.kx) / 2.0
-      rays.l[rijk] = (tvol.ln + tvol.lx) / 2.0
-      rays.m[rijk] = (tvol.mn + tvol.mx) / 2.0
-
-      rays.dxray[rijk] = tvol.xx - tvol.xn
-      rays.dyray[rijk] = tvol.yy - tvol.yn
-      rays.dzray[rijk] = tvol.zz - tvol.zn
-      rays.dkray[rijk] = tvol.kx - tvol.kn
-      rays.dlray[rijk] = tvol.lx - tvol.ln
-      rays.dmray[rijk] = tvol.mx - tvol.mn
-
-      rays.area_xk[rijk] = rays.dxray[rijk] * rays.dkray[rijk]
-      rays.area_yl[rijk] = rays.dyray[rijk] * rays.dlray[rijk]
-      rays.area_zm[rijk] = rays.dzray[rijk] * rays.dmray[rijk]
-      omir = intrinsic_frequency(rijk, branchr, rays)
-      rays.omega[rijk] = omir
-      # wave action density
-      # TODO: as functions
-      fcpspx = ifelse(sizex > 1, rays.area_xk[rijk], 1.0)
-      fcpspy = ifelse(sizey > 1, rays.area_yl[rijk], 1.0)
-      fcpspz = rays.area_zm[rijk]
-
-      if cons_merge == "wa"
-        wa = tvol.wa / (fcpspx * fcpspy * fcpspz)
-      else
-        wa = tvol.wa / (omir * fcpspx * fcpspy * fcpspz)
-      end
-      rays.dens[rijk] = wa
-    end
+    generate_merged_rvs!(nr_merge, ijk, volume_intervals, rays, domain)
+    replace_rayvolumes!(rays, ijk, nr_merge, nray_max)
   end
 
   # total number of rays after merge
   @views nray_after = sum(nray[i0:i1, j0:j1, k0:k1])
   nray_after = MPI.Allreduce(nray_before, +, comm)
   return nothing
-end
-
-function intrinsic_frequency(rijk, branchr, rays)
-  k, l, m = wavenumbers(rijk, rays)
-  h = sqrt(k^2 + l^2)
-  z = rays.z[rijk]
-  NNr = stratification(z, 1)
-
-  omir = branchr * sqrt(NNr * h^2 + f_cor_nd^2 * m^2) / sqrt(h^2 + m^2)
-  return omir
-end
-
-function ray_index(ir_k, ir_l, ir_m, nxray, nyray, nzray, sizex, sizey)
-  if sizex > 1
-    if sizey > 1
-      jray =
-        (ir_m - 1) * (nyray - 1) * (nxray - 1) + (ir_l - 1) * (nxray - 1) + ir_k
-    else
-      jray = (ir_m - 1) * (nxray - 1) + ir_k
-    end
-  else
-    if (sizey > 1)
-      jray = (ir_m - 1) * (nyray - 1) + ir_l
-    else
-      jray = ir_m
-    end
-  end
-  return jray
-end
-
-function index_klm(rijk, w, interval, n_ray)
-  if w < 0
-    if abs(log(-w / interval.max_n) / interval.mg_n) < 1.0
-      ir = n_ray / 2 - 1
-    else
-      ir = Int64(log(-w / interval.min_n) / interval.mg_n) + 1
-    end
-  elseif w == 0
-    ir = n_ray / 2
-  else
-    if abs(log(w / interval.max_p) / interval.mg_p < 1.0)
-      ir = n_ray - 1
-    else
-      ir = Int64(log(w / interval.min_p) / interval.mg_p) + n_ray / 2 + 1
-    end
-  end
-  return ir
 end
