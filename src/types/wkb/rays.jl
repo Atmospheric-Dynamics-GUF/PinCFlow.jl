69cfaad59900482f0f04794ce11f1a0d0eb0174b
Jonas Rothermel <j.rothermel@sinabis-analytics.com>
2025-04-08 15:33:31 +0200

    add ray getters

--- src/types/wkb/rays.jl
+++ src/types/wkb/rays.jl
@@ -21,3 +21,23 @@ end
 function Rays(nray_wrk::Integer, nxx::Integer, nyy::Integer, nzz::Integer)
   return Rays([zeros(nray_wrk, nxx, nyy, nzz) for i in 1:17]...)
 end
+
+function get_positions(rijk::CartesianIndex{4}, rays)
+  return rays.x[rijk], rays.y[rijk], rays.z[rijk]
+end
+
+function get_wavenumbers(rijk::CartesianIndex{4}, rays)
+  return rays.k[rijk], rays.l[rijk], rays.m[rijk]
+end
+
+function get_physical_extents(rijk::CartesianIndex{4}, rays)
+  return rays.dxray[rijk], rays.dyray[rijk], rays.dzray[rijk]
+end
+
+function get_spectral_extents(rijk::CartesianIndex{4}, rays)
+  return rays.dkray[rijk], rays.dlray[rijk], rays.dmray[rijk]
+end
+
+function get_areas(rijk::CartesianIndex{4}, rays)
+  return rays.area_xk[rijk], rays.area_yl[rijk], rays.area_zm[rijk]
+end
