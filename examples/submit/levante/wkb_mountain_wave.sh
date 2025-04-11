b0a3b740fe9b71e2959fd210668dfb4efb52c320
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-04-11 11:41:29 +0200

    Added a WKB-mountain-wave example. The plot shows results obtained with the Fortran code (to be replaced later).

---
+++ examples/submit/levante/wkb_mountain_wave.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --partition=compute
+#SBATCH --job-name=wkb_mountain_wave
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --hint=nomultithread
+#SBATCH --time=0-00:10:00
+#SBATCH --mail-type=FAIL
+#SBATCH --account=bb1097
+
+set -x
+
+# Set the number of tasks.
+ntasks=64
+
+# Define the work directory.
+user=$(whoami)
+scratch=/scratch/b/${user}/pinc/examples/wkb_mountain_wave
+mkdir -p ${scratch}
+
+# Configure MPI and HDF5.
+julia --project=../../../ -e 'using MPIPreferences; MPIPreferences.use_system_binary(; library_names=["/sw/spack-levante/intel-oneapi-mpi-2021.5.0-mrcss7/mpi/2021.5.0/lib/release/libmpi.so"])'
+julia --project=../../../ -e 'using HDF5; HDF5.API.set_libraries!("/sw/spack-levante/hdf5-1.12.1-jmeuy3/lib/libhdf5.so", "/sw/spack-levante/hdf5-1.12.1-jmeuy3/lib/libhdf5_hl.so")'
+
+# Run the model.
+mpiexec -n ${ntasks} julia --project=../../../ --check-bounds=no --math-mode=fast wkb_mountain_wave.jl 1>${scratch}/run.log 2>&1
+
+exit 0
