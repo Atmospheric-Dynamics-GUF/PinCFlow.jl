e84ffb55774331e27b0dd78ef03f950a95fe9c17
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-04-16 15:32:52 +0200

    Bug fixes in merge_rays!.

--- src/MSGWaM/RayOperations/compute_spectral_bounds.jl
+++ src/MSGWaM/RayOperations/compute_spectral_bounds.jl
@@ -3,8 +3,8 @@ function compute_spectral_bounds(wavenumbers::AbstractVector{<:AbstractFloat})
     # Compute minima and maxima.
     @views min_p = minimum(wavenumbers[wavenumbers .> 0.0]; init = 0.0)
     @views max_p = maximum(wavenumbers[wavenumbers .> 0.0]; init = 0.0)
-    @views min_n = maximum(wavenumbers[wavenumbers .< 0.0]; init = 0.0)
-    @views max_n = minimum(wavenumbers[wavenumbers .< 0.0]; init = 0.0)
+    @views min_n = minimum(-wavenumbers[wavenumbers .< 0.0]; init = 0.0)
+    @views max_n = maximum(-wavenumbers[wavenumbers .< 0.0]; init = 0.0)
 
     # Adjust bounds that are zero.
     if min_p == max_p == min_n == max_n == 0.0
@@ -22,12 +22,12 @@ function compute_spectral_bounds(wavenumbers::AbstractVector{<:AbstractFloat})
 
     # Prevent zero-width intervals.
     if min_n == max_n
-        min_n = 0.5 * min_n
-        max_n = 2.0 * max_n
+        min_n /= 2
+        max_n *= 2
     end
     if min_p == max_p
-        min_p = 0.5 * min_p
-        max_p = 2.0 * max_p
+        min_p /= 2
+        max_p *= 2
     end
 
     # Return.
