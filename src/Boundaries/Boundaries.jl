cc7564ab717726c36ea674e2bc2910b8ab50c032
Jonas Rothermel <j.rothermel@sinabis-analytics.com>
2025-04-17 15:04:29 +0200

    Move types to submodules

--- src/Boundaries/Boundaries.jl
+++ src/Boundaries/Boundaries.jl
@@ -3,6 +3,12 @@ module Boundaries
 using ..Types
 using ..MPIOperations
 
+struct BoundaryPredictands end
+struct BoundaryReconstructions end
+struct BoundaryFluxes end
+struct BoundaryGWIntegrals end
+struct BoundaryGWTendencies end
+
 include("set_boundaries!.jl")
 include("set_meridional_boundaries_of_field!.jl")
 include("set_meridional_boundaries_of_reduced_field!.jl")
@@ -19,6 +25,10 @@ export set_boundaries!,
     set_vertical_boundaries!,
     set_zonal_boundaries_of_field!,
     set_zonal_boundaries_of_reduced_field!,
-    set_zonal_boundaries!
-
+    set_zonal_boundaries!,
+    BoundaryPredictands,
+    BoundaryReconstructions,
+    BoundaryFluxes,
+    BoundaryGWIntegrals,
+    BoundaryGWTendencies
 end
