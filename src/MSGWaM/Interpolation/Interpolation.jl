6c119d4aa63cd278363887a5b6eca917b4a8288d
Jonas Rothermel <j.rothermel@sinabis-analytics.com>
2025-04-17 15:04:29 +0200

    Move types to submodules

--- src/MSGWaM/Interpolation/Interpolation.jl
+++ src/MSGWaM/Interpolation/Interpolation.jl
@@ -3,6 +3,15 @@ module Interpolation
 using ...Types
 using ...Update
 
+struct N2 <: AbstractVariable end
+struct DN2DZ <: AbstractVariable end
+struct DUDX <: AbstractVariable end
+struct DUDY <: AbstractVariable end
+struct DUDZ <: AbstractVariable end
+struct DVDX <: AbstractVariable end
+struct DVDY <: AbstractVariable end
+struct DVDZ <: AbstractVariable end
+
 include("get_next_half_level.jl")
 include("get_next_level.jl")
 include("interpolate_mean_flow.jl")
@@ -14,6 +23,7 @@ export get_next_half_level,
     get_next_level,
     interpolate_mean_flow,
     interpolate_sponge,
-    interpolate_stratification
+    interpolate_stratification,
+    N2
 
 end
--- src/Types/Types.jl
+++ src/Types/Types.jl
@@ -3,39 +3,14 @@ module Types
 abstract type AbstractVariable end
 abstract type AbstractIntegration end
 
-struct BoundaryPredictands end
-struct BoundaryReconstructions end
-struct BoundaryFluxes end
-struct BoundaryGWIntegrals end
-struct BoundaryGWTendencies end
-struct Total end
-struct Horizontal end
 struct Rho <: AbstractVariable end
 struct RhoP <: AbstractVariable end
 struct U <: AbstractVariable end
 struct V <: AbstractVariable end
 struct W <: AbstractVariable end
 struct PiP <: AbstractVariable end
-struct N2 <: AbstractVariable end
-struct DN2DZ <: AbstractVariable end
-struct DUDX <: AbstractVariable end
-struct DUDY <: AbstractVariable end
-struct DUDZ <: AbstractVariable end
-struct DVDX <: AbstractVariable end
-struct DVDY <: AbstractVariable end
-struct DVDZ <: AbstractVariable end
-struct Cartesian end
-struct TFC end
-struct LHS end
-struct RHS end
 struct EXPL <: AbstractIntegration end
 struct IMPL <: AbstractIntegration end
-struct X end
-struct Y end
-struct Z end
-struct XZ end
-struct YZ end
-struct XYZ end
 
 include("NamelistTypes/NamelistTypes.jl")
 include("FoundationalTypes/FoundationalTypes.jl")
@@ -50,16 +25,6 @@ using .VariableTypes
 using .WKBTypes
 
 include("State.jl")
-
-include("../MSGWaM/RaySources/activate_orographic_source!.jl")
-include("../MSGWaM/RaySources/compute_orographic_mode.jl")
-include("../MSGWaM/Interpolation/interpolate_stratification.jl")
-include("../MSGWaM/Interpolation/interpolate_mean_flow.jl")
-include("../MSGWaM/Interpolation/interpolate.jl")
-include("../MSGWaM/Interpolation/get_next_level.jl")
-include("../MSGWaM/Interpolation/get_next_half_level.jl")
-include("../Update/compute_vertical_wind.jl")
-include("../Update/transform.jl")
 
 export AbstractBackground,
     AbstractCoriolis,
@@ -76,12 +41,7 @@ export AbstractBackground,
     AbstractWKBTestCase,
     AbstractWKBFilter
 
-export BoundaryPredictands,
-    BoundaryReconstructions,
-    BoundaryFluxes,
-    BoundaryGWIntegrals,
-    BoundaryGWTendencies,
-    Total,
+export Total,
     Horizontal,
     Isothermal,
     ConstantCoriolis,
@@ -91,15 +51,8 @@ export BoundaryPredictands,
     U,
     V,
     W,
+    EXPL, IMPL,
     PiP,
-    N2,
-    DN2DZ,
-    DUDX,
-    DUDY,
-    DUDZ,
-    DVDX,
-    DVDY,
-    DVDZ,
     PseudoIncompressible,
     MountainWave,
     WKBMountainWave,
@@ -113,18 +66,6 @@ export BoundaryPredictands,
     ConstantWaveEnergy,
     Clip,
     Scale,
-    Cartesian,
-    TFC,
-    LHS,
-    RHS,
-    EXPL,
-    IMPL,
-    X,
-    Y,
-    Z,
-    XZ,
-    YZ,
-    XYZ,
     SteadyState,
     SingleColumn,
     MultiColumn,
