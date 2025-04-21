c7ec6bd8da15adeaed799c90d0b08f36414c1507
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-04-21 15:39:39 +0200

    Unified index computation.

--- src/MSGWaM/RayUpdate/split_rays!.jl
+++ src/MSGWaM/RayUpdate/split_rays!.jl
@@ -186,8 +186,8 @@ function split_rays!(
 
         dzr = rays.dzray[iray, ix, jy, kz]
 
-        ixrv = round(Int, (xr - lx[1] - dx / 2) / dx) + i0 - io
-        jyrv = round(Int, (yr - ly[1] - dy / 2) / dy) + j0 - jo
+        ixrv = floor(Int, (xr - lx[1]) / dx) + i0 - io
+        jyrv = floor(Int, (yr - ly[1]) / dy) + j0 - jo
         kzrvd = get_next_half_level(ixrv, jyrv, zr - 0.5 * dzr, domain, grid)
         kzrvu = get_next_half_level(ixrv, jyrv, zr + 0.5 * dzr, domain, grid)
 
