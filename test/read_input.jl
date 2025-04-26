4a9fe92a3258a3ff4af3b758e15f5367a6f1a70a
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-04-26 12:11:57 +0200

    Made some adjustments to the tests.

--- test/read_input.jl
+++ test/read_input.jl
@@ -82,92 +82,92 @@ namelists = Namelists(;
     wkb = wkb,
 )
 
-@testset "Read input" begin
-    state = State(namelists)
-    (; k0, k1, i0, i1, j0, j1) = state.domain
-    (; sizex, sizey, sizez) = state.namelists.domain
+state = State(namelists)
+(; k0, k1, i0, i1, j0, j1) = state.domain
+(; sizex, sizey, sizez) = state.namelists.domain
 
-    cpu_start_time = now()
-    PinCFlow.create_output(state)
+cpu_start_time = now()
+PinCFlow.create_output(state)
 
-    state.variables.predictands.u .=
-        rand(Float32, size(state.variables.predictands.u))
-    state.variables.predictands.v .=
-        rand(Float32, size(state.variables.predictands.v))
-    state.variables.predictands.w .=
-        rand(Float32, size(state.variables.predictands.w))
+state.variables.predictands.u .=
+    rand(Float32, size(state.variables.predictands.u))
+state.variables.predictands.v .=
+    rand(Float32, size(state.variables.predictands.v))
+state.variables.predictands.w .=
+    rand(Float32, size(state.variables.predictands.w))
 
-    # set rays
-    #
-    nray = 5
-    state.wkb.rays.dens .= 0.0
-    state.wkb.rays.x .= zeros(size(state.wkb.rays.x))
-    # make sure that rays.dens has some "holes", i.e. zero values
-    for kz in k0:k1, jy in j0:j1, ix in i0:i1
-        indices = randperm(state.wkb.nray_max)[1:nray]
-        state.wkb.rays.dens[indices, ix, jy, kz] .= 1.0
-        # mark some x values so that we can look for them later
-        state.wkb.rays.x[indices, ix, jy, kz] .= 1.0
-    end
-
-    dens_sum = sum(state.wkb.rays.dens)
-    @assert isapprox(dens_sum, sizex * sizey * sizez * nray)
-
-    PinCFlow.write_output(state, 0.0, 1, cpu_start_time)
-
-    restart_output = OutputNamelist(;
-        output_variables = (),
-        restart = true,
-        iin = 2,
-        input_file = "./pincflow_output.h5",
-        output_file = "./pincflow_output.h5",
-    )
-    in_state = PinCFlow.State(
-        Namelists(;
-            domain = domain,
-            output = restart_output,
-            setting = setting,
-            discretization = discretization,
-            poisson = poisson,
-            atmosphere = atmosphere,
-            grid = grid,
-            sponge = sponge,
-            wkb = wkb,
-        ),
-    )
-
-    t = PinCFlow.read_input!(in_state)
-
-    # check that fields match **inside** grid
-    (; i0, i1, j0, j1, k0, k1) = in_state.domain
-    @test all(
-        isapprox.(
-            in_state.variables.predictands.u[i0:i1, j0:j1, k0:k1],
-            state.variables.predictands.u[i0:i1, j0:j1, k0:k1],
-            rtol = 1e-6,
-        ),
-    )
-    @test all(
-        isapprox.(
-            in_state.variables.predictands.v[i0:i1, j0:j1, k0:k1],
-            state.variables.predictands.v[i0:i1, j0:j1, k0:k1],
-            rtol = 1e-6,
-        ),
-    )
-    @test all(
-        isapprox.(
-            in_state.variables.predictands.w[i0:i1, j0:j1, k0:k1],
-            state.variables.predictands.w[i0:i1, j0:j1, k0:k1],
-            rtol = 1e-6,
-        ),
-    )
-
-    @test isapprox(dens_sum, sum(in_state.wkb.rays.dens), rtol = 1e-6)
-    @test isapprox(sum(in_state.wkb.nray), dens_sum)
-
-    # find the marked x values and test that the shuffeling is correct
-    marked_x_values = findall(isapprox.(in_state.wkb.rays.x, 1.0, rtol = 1e-6))
-    foreach(marked_x_values) do i
-        @test isapprox(in_state.wkb.rays.dens[i], 1.0, rtol = 1e-6)
-    end
+# set rays
+#
+nray = 5
+state.wkb.rays.dens .= 0.0
+state.wkb.rays.x .= zeros(size(state.wkb.rays.x))
+# make sure that rays.dens has some "holes", i.e. zero values
+for kz in k0:k1, jy in j0:j1, ix in i0:i1
+    indices = randperm(state.wkb.nray_max)[1:nray]
+    state.wkb.rays.dens[indices, ix, jy, kz] .= 1.0
+    # mark some x values so that we can look for them later
+    state.wkb.rays.x[indices, ix, jy, kz] .= 1.0
 end
+
+dens_sum = sum(state.wkb.rays.dens)
+@assert isapprox(dens_sum, sizex * sizey * sizez * nray)
+
+PinCFlow.write_output(state, 0.0, 1, cpu_start_time)
+
+restart_output = OutputNamelist(;
+    output_variables = (),
+    restart = true,
+    iin = 2,
+    input_file = "./pincflow_output.h5",
+    output_file = "./pincflow_output.h5",
+)
+in_state = PinCFlow.State(
+    Namelists(;
+        domain = domain,
+        output = restart_output,
+        setting = setting,
+        discretization = discretization,
+        poisson = poisson,
+        atmosphere = atmosphere,
+        grid = grid,
+        sponge = sponge,
+        wkb = wkb,
+    ),
+)
+
+t = PinCFlow.read_input!(in_state)
+
+# check that fields match **inside** grid
+(; i0, i1, j0, j1, k0, k1) = in_state.domain
+@test all(
+    isapprox.(
+        in_state.variables.predictands.u[i0:i1, j0:j1, k0:k1],
+        state.variables.predictands.u[i0:i1, j0:j1, k0:k1],
+        rtol = 1e-6,
+    ),
+)
+@test all(
+    isapprox.(
+        in_state.variables.predictands.v[i0:i1, j0:j1, k0:k1],
+        state.variables.predictands.v[i0:i1, j0:j1, k0:k1],
+        rtol = 1e-6,
+    ),
+)
+@test all(
+    isapprox.(
+        in_state.variables.predictands.w[i0:i1, j0:j1, k0:k1],
+        state.variables.predictands.w[i0:i1, j0:j1, k0:k1],
+        rtol = 1e-6,
+    ),
+)
+
+@test isapprox(dens_sum, sum(in_state.wkb.rays.dens), rtol = 1e-6)
+@test isapprox(sum(in_state.wkb.nray), dens_sum)
+
+# find the marked x values and test that the shuffeling is correct
+marked_x_values = findall(isapprox.(in_state.wkb.rays.x, 1.0, rtol = 1e-6))
+foreach(marked_x_values) do i
+    @test isapprox(in_state.wkb.rays.dens[i], 1.0, rtol = 1e-6)
+end
+
+rm("pincflow_output.h5")
