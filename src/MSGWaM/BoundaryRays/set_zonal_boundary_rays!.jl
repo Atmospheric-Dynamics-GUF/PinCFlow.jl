949f0881df2ce39e92dfeea4ec050ddddad02be6
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-04-27 11:27:09 +0200

    Made some ray-volume loops more efficient.

--- src/MSGWaM/BoundaryRays/set_zonal_boundary_rays!.jl
+++ src/MSGWaM/BoundaryRays/set_zonal_boundary_rays!.jl
@@ -13,54 +13,42 @@ function set_zonal_boundary_rays!(state::State)
         set_zonal_halo_rays!(state)
     else
         for kz in (k0 - 1):(k1 + 1), jy in (j0 - 1):(j1 + 1)
-            if nray[i0 - 1, jy, kz] > 0
-                for iray in 1:nray[i0 - 1, jy, kz]
-                    copy_rays!(rays, (iray, i1, jy, kz), (iray, i0 - 1, jy, kz))
-                end
+            for iray in 1:nray[i0 - 1, jy, kz]
+                copy_rays!(rays, (iray, i1, jy, kz), (iray, i0 - 1, jy, kz))
             end
 
-            if nray[i1 + 1, jy, kz] > 0
-                for iray in 1:nray[i1 + 1, jy, kz]
-                    copy_rays!(rays, (iray, i0, jy, kz), (iray, i1 + 1, jy, kz))
-                end
+            for iray in 1:nray[i1 + 1, jy, kz]
+                copy_rays!(rays, (iray, i0, jy, kz), (iray, i1 + 1, jy, kz))
             end
         end
     end
 
     if io == 0
-        for kz in (k0 - 1):(k1 + 1), jy in (j0 - 1):(j1 + 1)
-            for ix in (i0 - 1):i0
-                if nray[ix, jy, kz] > 0
-                    for iray in 1:nray[ix, jy, kz]
-                        xr = rays.x[iray, ix, jy, kz]
-                        xrt = xr - lx[2] + lx[1]
+        for kz in (k0 - 1):(k1 + 1), jy in (j0 - 1):(j1 + 1), ix in (i0 - 1):i0
+            for iray in 1:nray[ix, jy, kz]
+                xr = rays.x[iray, ix, jy, kz]
+                xrt = xr - lx[2] + lx[1]
 
-                        if abs(xrt - x[io + ix]) < abs(xr - x[io + ix])
-                            xr = xrt
-                        end
-
-                        rays.x[iray, ix, jy, kz] = xr
-                    end
+                if abs(xrt - x[io + ix]) < abs(xr - x[io + ix])
+                    xr = xrt
                 end
+
+                rays.x[iray, ix, jy, kz] = xr
             end
         end
     end
 
     if io + nxx == sizexx
-        for kz in (k0 - 1):(k1 + 1), jy in (j0 - 1):(j1 + 1)
-            for ix in i1:(i1 + 1)
-                if nray[ix, jy, kz] > 0
-                    for iray in 1:nray[ix, jy, kz]
-                        xr = rays.x[iray, ix, jy, kz]
-                        xrt = xr + lx[2] - lx[1]
+        for kz in (k0 - 1):(k1 + 1), jy in (j0 - 1):(j1 + 1), ix in i1:(i1 + 1)
+            for iray in 1:nray[ix, jy, kz]
+                xr = rays.x[iray, ix, jy, kz]
+                xrt = xr + lx[2] - lx[1]
 
-                        if abs(xrt - x[io + ix]) < abs(xr - x[io + ix])
-                            xr = xrt
-                        end
-
-                        rays.x[iray, ix, jy, kz] = xr
-                    end
+                if abs(xrt - x[io + ix]) < abs(xr - x[io + ix])
+                    xr = xrt
                 end
+
+                rays.x[iray, ix, jy, kz] = xr
             end
         end
     end
