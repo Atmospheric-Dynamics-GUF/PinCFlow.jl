3beda911c592952ff837b422378ac3fd76591c6f
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-05-04 12:14:38 +0200

    Bug fix in the functions for vertical halos.

--- src/MPIOperations/set_vertical_halos_of_field!.jl
+++ src/MPIOperations/set_vertical_halos_of_field!.jl
@@ -2,6 +2,7 @@ function set_vertical_halos_of_field!(
     field::AbstractArray{<:AbstractFloat, 3},
     namelists::Namelists,
     domain::Domain,
+    zboundaries::SolidWallBoundaries,
 )
 
     # Get all necessary fields.
@@ -74,6 +75,7 @@ function set_vertical_halos_of_field!(
     field::AbstractArray{<:AbstractFloat, 5},
     namelists::Namelists,
     domain::Domain,
+    zboundaries::SolidWallBoundaries,
 )
 
     # Get all necessary fields.
