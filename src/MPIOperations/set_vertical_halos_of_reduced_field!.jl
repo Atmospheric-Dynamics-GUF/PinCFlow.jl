4489002c7c8b9d1aa479d6cf0b8400d93dde6937
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-05-05 13:39:32 +0200

    Follow-up bug fixes.

--- src/MPIOperations/set_vertical_halos_of_reduced_field!.jl
+++ src/MPIOperations/set_vertical_halos_of_reduced_field!.jl
@@ -1,5 +1,5 @@
 function set_vertical_halos_of_reduced_field!(
-    field::AbstractArray{<:AbstractFloat, 3},
+    field::AbstractArray{<:Real, 3},
     namelists::Namelists,
     domain::Domain,
     zboundaries::SolidWallBoundaries,
