2e3bab694c50f20b44bd8ae9e6841e856c8ff89b
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-05-05 17:31:36 +0200

    Removed auxiliary arrays from Domain. Rewrote boundary and halo functions to allow for a specific number of layers in each direction.

--- src/MPIOperations/set_vertical_halos_of_field!.jl
+++ src/MPIOperations/set_vertical_halos_of_field!.jl
@@ -1,64 +1,56 @@
 function set_vertical_halos_of_field!(
-    field::AbstractArray{<:AbstractFloat, 3},
+    field::AbstractArray{<:Real, 3},
     namelists::Namelists,
     domain::Domain,
-    zboundaries::SolidWallBoundaries,
+    zboundaries::SolidWallBoundaries;
+    layers::NTuple{3, <:Integer} = (-1, -1, -1),
 )
     (; nbz) = namelists.domain
-    (;
-        comm,
-        sizezz,
-        nzz,
-        ko,
-        k0,
-        k1,
-        down,
-        up,
-        send_f3_down,
-        send_f3_up,
-        recv_f3_down,
-        recv_f3_up,
-    ) = domain
+    (; comm, sizezz, nzz, nx, ny, ko, i0, i1, j0, j1, k0, k1, down, up) = domain
+
+    nbx = layers[1] == -1 ? namelists.domain.nbx : layers[1]
+    nby = layers[2] == -1 ? namelists.domain.nby : layers[2]
+    nbz = layers[3] == -1 ? namelists.domain.nbz : layers[3]
+
+    (send_up, send_down, receive_up, receive_down) =
+        (zeros(nx + 2 * nbx, ny + 2 * nby, nbz) for i in 1:4)
+
+    i = (i0 - nbx):(i1 + nbx)
+    j = (j0 - nby):(j1 + nby)
 
     if ko == 0
         for k in 1:nbz
-            @views send_f3_up[:, :, k] .= field[:, :, k1 - k + 1]
+            @views send_up[:, :, k] .= field[i, j, k1 - k + 1]
         end
 
-        MPI.Sendrecv!(send_f3_up, recv_f3_up, comm; dest = up, source = up)
+        MPI.Sendrecv!(send_up, receive_up, comm; dest = up, source = up)
 
         for k in 1:nbz
-            @views field[:, :, k1 + k] .= recv_f3_up[:, :, k]
+            @views field[i, j, k1 + k] .= receive_up[:, :, k]
         end
     elseif ko + nzz == sizezz
         for k in 1:nbz
-            @views send_f3_down[:, :, k] .= field[:, :, k0 + k - 1]
+            @views send_down[:, :, k] .= field[i, j, k0 + k - 1]
         end
 
-        MPI.Sendrecv!(
-            send_f3_down,
-            recv_f3_down,
-            comm;
-            dest = down,
-            source = down,
-        )
+        MPI.Sendrecv!(send_down, receive_down, comm; dest = down, source = down)
 
         for k in 1:nbz
-            @views field[:, :, k0 - k] .= recv_f3_down[:, :, k]
+            @views field[i, j, k0 - k] .= receive_down[:, :, k]
         end
     else
         for k in 1:nbz
-            @views send_f3_up[:, :, k] .= field[:, :, k1 - k + 1]
-            @views send_f3_down[:, :, k] .= field[:, :, k0 + k - 1]
+            @views send_up[:, :, k] .= field[i, j, k1 - k + 1]
+            @views send_down[:, :, k] .= field[i, j, k0 + k - 1]
         end
 
-        MPI.Sendrecv!(send_f3_up, recv_f3_down, comm; dest = up, source = down)
+        MPI.Sendrecv!(send_up, receive_down, comm; dest = up, source = down)
 
-        MPI.Sendrecv!(send_f3_down, recv_f3_up, comm; dest = down, source = up)
+        MPI.Sendrecv!(send_down, receive_up, comm; dest = down, source = up)
 
         for k in 1:nbz
-            @views field[:, :, k0 - k] .= recv_f3_down[:, :, k]
-            @views field[:, :, k1 + k] .= recv_f3_up[:, :, k]
+            @views field[i, j, k0 - k] .= receive_down[:, :, k]
+            @views field[i, j, k1 + k] .= receive_up[:, :, k]
         end
     end
 
@@ -69,63 +61,55 @@ function set_vertical_halos_of_field!(
     field::AbstractArray{<:AbstractFloat, 5},
     namelists::Namelists,
     domain::Domain,
-    zboundaries::SolidWallBoundaries,
+    zboundaries::SolidWallBoundaries;
+    layers::NTuple{3, <:Integer} = (-1, -1, -1),
 )
     (; nbz) = namelists.domain
-    (;
-        comm,
-        sizezz,
-        nzz,
-        ko,
-        k0,
-        k1,
-        down,
-        up,
-        send_f5_down,
-        send_f5_up,
-        recv_f5_down,
-        recv_f5_up,
-    ) = domain
+    (; comm, sizezz, nzz, nx, ny, ko, i0, i1, j0, j1, k0, k1, down, up) = domain
+
+    nbx = layers[1] == -1 ? namelists.domain.nbx : layers[1]
+    nby = layers[2] == -1 ? namelists.domain.nby : layers[2]
+    nbz = layers[3] == -1 ? namelists.domain.nbz : layers[3]
+
+    (send_up, send_down, receive_up, receive_down) =
+        (zeros(nx + 2 * nbx, ny + 2 * nby, nbz, 3, 2) for i in 1:4)
+
+    i = (i0 - nbx):(i1 + nbx)
+    j = (j0 - nby):(j1 + nby)
 
     if ko == 0
         for k in 1:nbz
-            @views send_f5_up[:, :, k, :, :] .= field[:, :, k1 - k + 1, :, :]
+            @views send_up[:, :, k, :, :] .= field[i, j, k1 - k + 1, :, :]
         end
 
-        MPI.Sendrecv!(send_f5_up, recv_f5_up, comm; dest = up, source = up)
+        MPI.Sendrecv!(send_up, receive_up, comm; dest = up, source = up)
 
         for k in 1:nbz
-            @views field[:, :, k1 + k, :, :] .= recv_f5_up[:, :, k, :, :]
+            @views field[i, j, k1 + k, :, :] .= receive_up[:, :, k, :, :]
         end
     elseif ko + nzz == sizezz
         for k in 1:nbz
-            @views send_f5_down[:, :, k, :, :] .= field[:, :, k0 + k - 1, :, :]
+            @views send_down[:, :, k, :, :] .= field[i, j, k0 + k - 1, :, :]
         end
 
-        MPI.Sendrecv!(
-            send_f5_down,
-            recv_f5_down,
-            comm;
-            dest = down,
-            source = down,
-        )
+        MPI.Sendrecv!(send_down, receive_down, comm; dest = down, source = down)
 
         for k in 1:nbz
-            @views field[:, :, k0 - k, :, :] .= recv_f5_down[:, :, k, :, :]
+            @views field[i, j, k0 - k, :, :] .= receive_down[:, :, k, :, :]
         end
     else
         for k in 1:nbz
-            @views send_f5_up[:, :, k, :, :] .= field[:, :, k1 - k + 1, :, :]
-            @views send_f5_down[:, :, k, :, :] .= field[:, :, k0 + k - 1, :, :]
+            @views send_up[:, :, k, :, :] .= field[i, j, k1 - k + 1, :, :]
+            @views send_down[:, :, k, :, :] .= field[i, j, k0 + k - 1, :, :]
         end
 
-        MPI.Sendrecv!(send_f5_up, recv_f5_down, comm; dest = up, source = down)
+        MPI.Sendrecv!(send_up, receive_down, comm; dest = up, source = down)
 
-        MPI.Sendrecv!(send_f5_down, recv_f5_up, comm; dest = down, source = up)
+        MPI.Sendrecv!(send_down, receive_up, comm; dest = down, source = up)
 
         for k in 1:nbz
-            @views field[:, :, k0 - k, :, :] .= recv_f5_down[:, :, k, :, :]
-            @views field[:, :, k1 + k, :, :] .= recv_f5_up[:, :, k, :, :]
+            @views field[i, j, k0 - k, :, :] .= receive_down[:, :, k, :, :]
+            @views field[i, j, k1 + k, :, :] .= receive_up[:, :, k, :, :]
         end
     end
 
--- src/Types/FoundationalTypes/Domain.jl
+++ src/Types/FoundationalTypes/Domain.jl
@@ -1,18 +1,4 @@
-struct Domain{
-    A <: MPI.Comm,
-    B <: Bool,
-    C <: Integer,
-    D <: AbstractArray{<:AbstractFloat, 3},
-    E <: AbstractArray{<:AbstractFloat, 5},
-    F <: AbstractArray{<:AbstractFloat, 3},
-    G <: AbstractArray{<:AbstractFloat, 5},
-    H <: AbstractArray{<:AbstractFloat, 3},
-    I <: AbstractArray{<:AbstractFloat, 5},
-    J <: AbstractMatrix{<:AbstractFloat},
-    K <: AbstractMatrix{<:AbstractFloat},
-    L <: AbstractMatrix{<:AbstractFloat},
-    M <: AbstractVector{<:AbstractFloat},
-}
+struct Domain{A <: MPI.Comm, B <: Bool, C <: Integer}
 
     # MPI variables.
     comm::A
@@ -55,50 +41,6 @@ struct Domain{
     forw::C
     down::C
     up::C
-
-    # Auxiliary arrays for setting halos of a field.
-    send_f3_left::D
-    send_f3_right::D
-    recv_f3_left::D
-    recv_f3_right::D
-    send_f5_left::E
-    send_f5_right::E
-    recv_f5_left::E
-    recv_f5_right::E
-    send_f3_back::F
-    send_f3_forw::F
-    recv_f3_back::F
-    recv_f3_forw::F
-    send_f5_back::G
-    send_f5_forw::G
-    recv_f5_back::G
-    recv_f5_forw::G
-    send_f3_down::H
-    send_f3_up::H
-    recv_f3_down::H
-    recv_f3_up::H
-    send_f5_down::I
-    send_f5_up::I
-    recv_f5_down::I
-    recv_f5_up::I
-
-    # Auxiliary arrays for setting halos of a reduced field.
-    send_rf3_left::J
-    send_rf3_right::J
-    recv_rf3_left::J
-    recv_rf3_right::J
-    send_rf3_back::K
-    send_rf3_forw::K
-    recv_rf3_back::K
-    recv_rf3_forw::K
-    send_rf3_down::L
-    send_rf3_up::L
-    recv_rf3_down::L
-    recv_rf3_up::L
-
-    # Auxiliary arrays for horizontal averaging.
-    local_sum::M
-    global_sum::M
 end
 
 function Domain(namelists::Namelists)
@@ -186,31 +128,6 @@ function Domain(namelists::Namelists)
     (back, forw) = MPI.Cart_shift(comm, 1, 1)
     (down, up) = MPI.Cart_shift(comm, 2, 1)
 
-    # Initialize auxiliary arrays for setting all halo layers.
-    (send_f3_left, send_f3_right, recv_f3_left, recv_f3_right) =
-        (zeros(nbx, nyy, nzz) for i in 1:4)
-    (send_f5_left, send_f5_right, recv_f5_left, recv_f5_right) =
-        (zeros(nbx, nyy, nzz, 3, 2) for i in 1:4)
-    (send_f3_back, send_f3_forw, recv_f3_back, recv_f3_forw) =
-        (zeros(nxx, nby, nzz) for i in 1:4)
-    (send_f5_back, send_f5_forw, recv_f5_back, recv_f5_forw) =
-        (zeros(nxx, nby, nzz, 3, 2) for i in 1:4)
-    (send_f3_down, send_f3_up, recv_f3_down, recv_f3_up) =
-        (zeros(nxx, nyy, nbz) for i in 1:4)
-    (send_f5_down, send_f5_up, recv_f5_down, recv_f5_up) =
-        (zeros(nxx, nyy, nbz, 3, 2) for i in 1:4)
-
-    # Initialize auxiliary arrays for setting one halo layer.
-    (send_rf3_left, send_rf3_right, recv_rf3_left, recv_rf3_right) =
-        (zeros(ny + 2, nz + 2) for i in 1:4)
-    (send_rf3_back, send_rf3_forw, recv_rf3_back, recv_rf3_forw) =
-        (zeros(nx + 2, nz + 2) for i in 1:4)
-    (send_rf3_down, send_rf3_up, recv_rf3_down, recv_rf3_up) =
-        (zeros(nx + 2, ny + 2) for i in 1:4)
-
-    # Initialize auxiliary arrays for horizontal averaging.
-    (local_sum, global_sum) = (zeros(sizez) for i in 1:2)
-
     # Return Domain instance.
     return Domain(
         comm,
@@ -241,43 +158,5 @@ function Domain(namelists::Namelists)
         forw,
         down,
         up,
-        send_f3_left,
-        send_f3_right,
-        recv_f3_left,
-        recv_f3_right,
-        send_f5_left,
-        send_f5_right,
-        recv_f5_left,
-        recv_f5_right,
-        send_f3_back,
-        send_f3_forw,
-        recv_f3_back,
-        recv_f3_forw,
-        send_f5_back,
-        send_f5_forw,
-        recv_f5_back,
-        recv_f5_forw,
-        send_f3_down,
-        send_f3_up,
-        recv_f3_down,
-        recv_f3_up,
-        send_f5_down,
-        send_f5_up,
-        recv_f5_down,
-        recv_f5_up,
-        send_rf3_left,
-        send_rf3_right,
-        recv_rf3_left,
-        recv_rf3_right,
-        send_rf3_back,
-        send_rf3_forw,
-        recv_rf3_back,
-        recv_rf3_forw,
-        send_rf3_down,
-        send_rf3_up,
-        recv_rf3_down,
-        recv_rf3_up,
-        local_sum,
-        global_sum,
     )
 end
