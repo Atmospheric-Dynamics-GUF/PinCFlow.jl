1f196b714d7988066d95bdfc690458e84d69ad64
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-05-10 22:07:28 +0200

    Improved example batch scripts.

--- examples/submit/levante/mountain_wave.sh
+++ examples/submit/levante/mountain_wave.sh
@@ -10,8 +10,9 @@
 
 set -x
 
-# Set the number of tasks.
-ntasks=64
+# Set Intel MPI configuration.
+export I_MPI_PMI=pmi
+export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
 
 # Define the work directory.
 user=$(whoami)
@@ -23,6 +24,6 @@ julia --project=../../../ -e 'using MPIPreferences; MPIPreferences.use_system_bi
 julia --project=../../../ -e 'using HDF5; HDF5.API.set_libraries!("/sw/spack-levante/hdf5-1.12.1-jmeuy3/lib/libhdf5.so", "/sw/spack-levante/hdf5-1.12.1-jmeuy3/lib/libhdf5_hl.so")'
 
 # Run the model.
-mpiexec -n ${ntasks} julia --project=../../../ --check-bounds=no --math-mode=fast mountain_wave.jl 1>${scratch}/run.log 2>&1
+srun --cpu_bind=verbose --distribution=block:cyclic julia --project=../../../ --check-bounds=no --math-mode=fast mountain_wave.jl 1>${scratch}/run.log 2>&1
 
 exit 0
