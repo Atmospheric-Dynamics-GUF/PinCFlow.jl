112e54d480ff351ee196809f1266baae4e3abcd8
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-05-20 00:10:20 +0200

    Adjusted get_next_half_level and get_next_level to avoid out-of-bounds errors when using vertical parallelization.

--- src/MSGWaM/Interpolation/get_next_level.jl
+++ src/MSGWaM/Interpolation/get_next_level.jl
@@ -5,18 +5,24 @@ function get_next_level(
     domain::Domain,
     grid::Grid,
 )
-    (; sizezz, nzz, nz, ko, k0, k1) = domain
+    (; sizezz, nzz, ko, k0, k1) = domain
     (; ztfc) = grid
 
     @views k = argmin(abs.(ztfc[i, j, :] .- z))
-    if ztfc[i, j, k] < z && k < nzz
+    if ztfc[i, j, k] < z
         k += 1
     end
-    if ko + k < k0
-        k = k0
+
+    if ko == 0
+        k = max(k, k0)
+    else
+        k = max(k, 3)
     end
-    if ko + k > sizezz - (nzz - nz) / 2 + 1
-        k = k1 + 1
+
+    if ko + nzz == sizezz
+        k = min(k, k1 + 1)
+    else
+        k = min(k, nzz - 1)
     end
 
     return k
