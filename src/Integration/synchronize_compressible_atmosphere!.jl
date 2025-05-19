4fdfa7f81125f247620f711321e31a54e1073295
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-05-19 21:19:17 +0200

    Fixed the computation of the boundary values of the squared buoyancy frequency (this bug only affected simulations with npz>1).

--- src/Integration/synchronize_compressible_atmosphere!.jl
+++ src/Integration/synchronize_compressible_atmosphere!.jl
@@ -20,18 +20,32 @@ function synchronize_compressible_atmosphere!(
     predictands::Predictands,
     model::Compressible,
 )
-    (; nbz) = state.namelists.domain
+    (; namelists, domain) = state
+    (; nbz) = namelists.domain
+    (; zboundaries) = namelists.setting
     (; g_ndim) = state.constants
-    (; sizezz, nzz, ko, k0, k1) = state.domain
+    (; sizezz, nzz, ko, k0, k1) = domain
     (; dz, jac) = state.grid
     (; pstrattfc, bvsstrattfc, thetastrattfc, rhostrattfc) = state.atmosphere
     (; rho, p) = predictands
 
     pstrattfc .= p
 
-    kz0 = ko == 0 ? k0 : 1
-    kz1 = ko + nzz == sizezz ? k1 : nzz
+    for kz in k0:k1
+        bvsstrattfc[:, :, kz] .=
+            g_ndim .* p[:, :, kz] ./ (rho[:, :, kz] .+ rhostrattfc[:, :, kz]) ./
+            (thetastrattfc[:, :, kz] .^ 2) ./ jac[:, :, kz] .*
+            (thetastrattfc[:, :, kz + 1] .- thetastrattfc[:, :, kz - 1]) ./ 2 ./
+            dz
+    end
 
+    set_vertical_boundaries_of_field!(
+        bvsstrattfc,
+        namelists,
+        domain,
+        zboundaries,
+        +,
+    )
     if ko == 0
         for k in 1:nbz
             bvsstrattfc[:, :, k] .=
@@ -41,22 +55,13 @@ function synchronize_compressible_atmosphere!(
                 (thetastrattfc[:, :, k0] .- thetastrattfc[:, :, k0 - 1]) ./ dz
         end
     end
-
-    for kz in kz0:kz1
-        bvsstrattfc[:, :, kz] .=
-            g_ndim .* p[:, :, kz] ./ (rho[:, :, kz] .+ rhostrattfc[:, :, kz]) ./
-            (thetastrattfc[:, :, kz] .^ 2) ./ jac[:, :, kz] .*
-            (thetastrattfc[:, :, kz + 1] .- thetastrattfc[:, :, kz - 1]) ./ 2 ./
-            dz
-    end
-
     if ko + nzz == sizezz
         for k in 1:nbz
-        bvsstrattfc[:, :, k1 + k] .=
-            g_ndim .* p[:, :, k1 + 1] ./
-            (rho[:, :, k1 + 1] .+ rhostrattfc[:, :, k1 + 1]) ./
-            (thetastrattfc[:, :, k1 + 1] .^ 2) ./ jac[:, :, k1 + 1] .*
-            (thetastrattfc[:, :, k1 + 1] .- thetastrattfc[:, :, k1]) ./ dz
+            bvsstrattfc[:, :, k1 + k] .=
+                g_ndim .* p[:, :, k1 + 1] ./
+                (rho[:, :, k1 + 1] .+ rhostrattfc[:, :, k1 + 1]) ./
+                (thetastrattfc[:, :, k1 + 1] .^ 2) ./ jac[:, :, k1 + 1] .*
+                (thetastrattfc[:, :, k1 + 1] .- thetastrattfc[:, :, k1]) ./ dz
         end
     end
 
