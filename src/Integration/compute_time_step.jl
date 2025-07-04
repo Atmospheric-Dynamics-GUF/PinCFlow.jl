18e3a4bbb5676f0b6fc0279a2937823ea474cb46
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-07-04 10:01:53 +0200

    More adjustments to the docstring of compute_time_step.

--- src/Integration/compute_time_step.jl
+++ src/Integration/compute_time_step.jl
@@ -6,26 +6,28 @@ compute_time_step(state::State)
 Compute adaptive time step based on several stability criteria.
 
 If `state.namelists.discretization.adaptive_time_step` is set to `true`, the returned time step is given by
+
 ```math
 \\Delta t = \\min \\left(\\Delta t_\\mathrm{CFL}, \\Delta t_\\mathrm{WKB}, \\Delta t_\\mathrm{viscous}, \\Delta t_{\\max}\\right),
 ```
+
 where ``\\Delta t_\\mathrm{CFL}`` and ``\\Delta t_\\mathrm{WKB}`` are computed from CFL conditions with respect to the resolved flow and unresolved gravity waves, respectively, ``\\Delta t_\\mathrm{viscous}`` is determined from a von Neumann condition that takes the viscosity into account and ``\\Delta t_{\\max}`` is an upper limit specified in `state.namelists.discretization`. Otherwise, the returned time step is equal to ``\\Delta t_{\\max}``. If ``\\Delta t`` is smaller than ``\\Delta t_{\\min}`` (also specified in `state.namelists.discretization`), an error is thrown.
 
-# Stability constraints
+The individual stability criteria are as follows.
 
-## CFL condition with respect to the resolved flow
+  - CFL condition with respect to the resolved flow (where ``w`` is computed with `compute_vertical_wind`):
 
 ```math
 \\Delta t_\\mathrm{CFL} = \\mu_\\mathrm{CFL} \\min\\limits_\\mathrm{global} \\left[\\frac{\\Delta \\widehat{x}}{u_{\\max}}, \\frac{\\Delta \\widehat{y}}{v_{\\max}}, \\min \\left(\\frac{J \\Delta \\widehat{z}}{w}\\right)\\right]
 ```
 
-## CFL condition with respect to the group velocities of unresolved gravity waves
+  - CFL condition with respect to the (grid-cell) maxima of the group velocities of unresolved gravity waves (with ``J_{\\min}`` being the minimum Jacobian in a one-grid-cell radius):
 
 ```math
-\\Delta t_\\mathrm{WKB} = \\mu_\\mathrm{WKB} \\min\\limits_\\mathrm{global} \\left[\\frac{\\Delta \\widehat{x}}{c_{\\mathrm{g} x, \\max}}, \\frac{\\Delta \\widehat{y}}{c_{\\mathrm{g} y, \\max}}, \\min \\left(\\frac{J \\Delta \\widehat{z}}{c_{\\mathrm{g} z, \\max}}\\right)\\right]
+\\Delta t_\\mathrm{WKB} = \\mu_\\mathrm{WKB} \\min\\limits_\\mathrm{global} \\left[\\frac{\\Delta \\widehat{x}}{c_{\\mathrm{g} x, \\max}}, \\frac{\\Delta \\widehat{y}}{c_{\\mathrm{g} y, \\max}}, \\min \\left(\\frac{J_{\\min} \\Delta \\widehat{z}}{c_{\\mathrm{g} z}}\\right)\\right]
 ```
 
-## Von Neumann condition
+  - Von Neumann condition (with ``\\mathrm{Re}`` being the Reynolds number):
 
 ```math
 \\Delta t_\\mathrm{viscous} = \\frac{\\mathrm{Re}}{2} \\min\\limits_\\mathrm{global} \\left[\\left(\\Delta \\widehat{x}\\right)^2, \\left(\\Delta \\widehat{y}\\right)^2, \\left(J \\Delta \\widehat{z}\\right)^2\\right]
