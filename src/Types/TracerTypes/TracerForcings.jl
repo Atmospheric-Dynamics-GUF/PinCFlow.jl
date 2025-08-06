9f15f04216ff3ea0886920f81f232a5d22686163
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-08-06 12:18:17 +0000

    Corrected a typo in src/Types/TracerTypes/TracerForcings.jl.

--- src/Types/TracerTypes/TracerForcings.jl
+++ src/Types/TracerTypes/TracerForcings.jl
@@ -8,7 +8,7 @@ TracerForcings{
 ```
 
 ```julia
-TracerForcing(namelists::Namelists, domain::Domain)
+TracerForcings(namelists::Namelists, domain::Domain)
 ```
 """
 struct TracerForcings{
@@ -27,7 +27,7 @@ struct TracerForcings{
     chiturbw::C
 end
 
-function TracerForcing(namelists::Namelists, domain::Domain)
+function TracerForcings(namelists::Namelists, domain::Domain)
     (; tracersetup) = namelists.tracer
 
     return
