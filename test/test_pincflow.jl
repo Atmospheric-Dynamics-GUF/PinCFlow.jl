ab90db899ed348fb1fcce9a8b10eb6d6ad331e84
Marco Artiano <artianomarco97@gmail.com>
2025-09-26 12:09:03 +0200

    add Felix suggestion

--- test/test_pincflow.jl
+++ test/test_pincflow.jl
@@ -27,3 +27,40 @@ macro test_pinc_include(expr, args...)
         @test_trixi_include_base($(esc(expr)), $(args...))
     end
 end
+
+
+function test_example(file::AbstractString, args...)
+
+    local l2 = get_kwarg(args, :l2, nothing)
+    local linf = get_kwarg(args, :linf, nothing)
+    atol_default = 500 * eps(RealT)
+    rtol_default = sqrt(eps(RealT))
+    local atol = get_kwarg(args, :atol, atol_default)
+    local rtol = get_kwarg(args, :rtol, rtol_default)
+
+    script = read(file, String)
+    modified_script = replace(
+        script,
+        r"\bsizex *= *\d+\b" => "sizex = 3",
+        r"\bsizey *= *\d+\b" => "sizey = 3",
+        r"\bsizez *= *\d+\b" => "sizez = 3",
+        r"\bnpx *= *\d+\b" => "npx = 1",
+        r"\bnpy *= *\d+\b" => "npy = 1",
+        r"\bnpz *= *\d+\b" => "npz = 1",
+    )
+    eval(Meta.parseall(modified_script))
+
+    l2_measured, linf_measured = invokelatest((@__MODULE__).PinCFlow.analysis_callback,
+                                                      (@__MODULE__).sol)
+	 @test length($l2) == length(l2_measured)
+                for (l2_expected, l2_actual) in zip($l2, l2_measured)
+                    @test isapprox(l2_expected, l2_actual, atol = $atol, rtol = $rtol)
+                end
+	
+	@test length($linf) == length(linf_measured)
+                for (linf_expected, linf_actual) in zip($linf, linf_measured)
+                    @test isapprox(linf_expected, linf_actual, atol = $atol, rtol = $rtol)
+                end
+
+end
+
