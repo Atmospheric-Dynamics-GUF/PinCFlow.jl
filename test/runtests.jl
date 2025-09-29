41adeec68fac4177f795ddb3c8d5d99e9b72704a
Marco Artiano <artianomarco97@gmail.com>
2025-09-29 17:28:12 +0200

    rename test_mountain.jl to test_periodic_hill.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,6 @@ const pincflow_test = get(ENV, "PinCFlow_TEST", "all")
 
 @time @testset verbose = true showtiming = true "PinCFlow.jl tests" begin
     @time if pincflow_test == "all" ## || TODO: 
-        include("test_mountain.jl")
+        include("test_periodic_hill.jl")
     end
 end #end tests
