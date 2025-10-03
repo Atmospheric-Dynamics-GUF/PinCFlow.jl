e8a61791b603fbc26859b24b3a3848f5d7c4a473
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-10-03 11:58:01 +0200

    Made LaTeX fonts the default in the visualization theme.

--- ext/PinCFlowMakieExt/set_visualization_theme!.jl
+++ ext/PinCFlowMakieExt/set_visualization_theme!.jl
@@ -2,12 +2,6 @@ function set_visualization_theme!()
     color = [:blue, :red]
     set_theme!(
         theme_latexfonts();
-        fonts = (;
-        regular = "Times New Roman",
-        bold = "Times New Roman Bold",
-        italic = "Times New Roman Italic",
-        bold_italic = "Times New Roman Bold Italic",
-    ),
         Axis = (
             width = 200,
             height = 200,
