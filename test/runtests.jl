d76e591e16a664f3447037e79e845c399052094a
Felix Jochum <jochum@iau.uni-frankfurt.de>
2025-10-08 16:26:56 +0200

    Added a periodic-hill test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,5 +7,6 @@ submit_directory = "../examples/submit/"
 
 @time @testset verbose = true showtiming = true "PinCFlow tests" begin
     include("test_mountain_wave.jl")
+    include("test_periodic_hill.jl")
     include("test_wkb_mountain_wave.jl")
 end
