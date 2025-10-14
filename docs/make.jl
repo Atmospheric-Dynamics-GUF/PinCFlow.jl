e4917b3c4bdd295e033db979892721988ed0a8f9
Hendrik Ranocha <mail@ranocha.de>
2025-10-14 16:43:30 +0200

    changelog etc.

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,7 @@ using Pkg
 
 Pkg.activate("docs")
 
+using Changelog: Changelog
 using Documenter
 using Revise
 using PinCFlow
@@ -39,8 +40,67 @@ for file in readdir("examples/results/"; join = true)
     cp(file, "docs/src/" * file; force = true)
 end
 
-# Copy the README file.
-cp("README.md", "docs/src/index.md"; force = true)
+# Copy the README file and use it as landing page of the docs.
+cp(joinpath(dirname(@__DIR__), "README.md"), "docs/src/index.md"; force = true)
+
+# Copy list of authors to not need to synchronize it manually.
+authors_text = read(joinpath(dirname(@__DIR__), "AUTHORS.md"), String)
+authors_text = replace(authors_text,
+                       "in the [LICENSE.md](LICENSE.md) file" => "under [License](@ref)")
+write(joinpath(@__DIR__, "src", "authors.md"), authors_text)
+
+# Copy some files from the repository root directory to the docs and modify them
+# as necessary
+open(joinpath(@__DIR__, "src", "code_of_conduct.md"), "w") do io
+    # Point to source license file
+    println(io,
+            """
+            ```@meta
+            EditURL = "https://github.com/Atmospheric-Dynamics-GUF/PinCFlow.jl/blob/main/CODE_OF_CONDUCT.md"
+            ```
+            """)
+    # Write the modified contents
+    println(io, "# [Code of Conduct](@id code-of-conduct)")
+    println(io, "")
+    for line in eachline(joinpath(dirname(@__DIR__), "CODE_OF_CONDUCT.md"))
+        line = replace(line, "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref)")
+        println(io, "> ", line)
+    end
+end
+
+open(joinpath(@__DIR__, "src", "contributing.md"), "w") do io
+    # Point to source license file
+    println(io,
+            """
+            ```@meta
+            EditURL = "https://github.com/Atmospheric-Dynamics-GUF/PinCFlow.jl/blob/main/CONTRIBUTING.md"
+            ```
+            """)
+    # Write the modified contents
+    for line in eachline(joinpath(dirname(@__DIR__), "CONTRIBUTING.md"))
+        line = replace(line, "[LICENSE.md](LICENSE.md)" => "[License](@ref)")
+        line = replace(line, "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref)")
+        println(io, line)
+    end
+end
+
+# Create changelog
+Changelog.generate(Changelog.Documenter(),                        # output type
+                   joinpath(@__DIR__, "..", "NEWS.md"),           # input file
+                   joinpath(@__DIR__, "src", "changelog_tmp.md"); # output file
+                   repo = "Atmospheric-Dynamics-GUF/PinCFlow.jl", # default repository for links
+                   branch = "main",)
+# Fix edit URL of changelog
+open(joinpath(@__DIR__, "src", "changelog.md"), "w") do io
+    for line in eachline(joinpath(@__DIR__, "src", "changelog_tmp.md"))
+        if startswith(line, "EditURL")
+            line = "EditURL = \"https://github.com/Atmospheric-Dynamics-GUF/PinCFlow.jl/blob/main/NEWS.md\""
+        end
+        println(io, line)
+    end
+end
+# Remove temporary file
+rm(joinpath(@__DIR__, "src", "changelog_tmp.md"))
 
 # Generate documentation.
 makedocs(;
@@ -69,6 +129,11 @@ makedocs(;
             "Output" => "reference/output.md",
         ],
         "Developer guide" => "developer_guide.md",
+        "Changelog" => "changelog.md",
+        "Authors" => "authors.md",
+        "Contributing" => "contributing.md",
+        "Code of Conduct" => "code_of_conduct.md",
+        "License" => "license.md"
     ],
     pagesonly = true,
     format = Documenter.HTML(;
