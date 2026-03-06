6a8539429b29573b8aba75787fa3a2c07702fb71
Skumar-goethe <s.kumar@iau.uni-frankfurt.de>
2026-03-06 09:23:11 +0100

    Multithread massage in initialisation

--- src/MSGWaM/TriadInteractions/initialize_wave_spectrum!.jl
+++ src/MSGWaM/TriadInteractions/initialize_wave_spectrum!.jl
@@ -17,6 +17,19 @@ end
 function initialize_wave_spectrum!(state::State, 
     wkb_mode::Union{MultiColumn, SingleColumn, SteadyState}, 
     triad_mode::Union{Triad2D, Triad3DIso})
+
+    (; master) = state.domain
+    (; nthreads_triad) = state.namelists.triad
+
+    if master
+        println(repeat("-", 80))
+        println("")
+        println("Triad interaction module activated with the model: ", triad_mode)
+        println("Number of threads per rank: (nthreads_triad) = ", nthreads_triad)
+        println("")
+        println(repeat("-", 80))
+    end
+
     get_wave_spectrum!(state, wkb_mode, triad_mode)
 end
 
