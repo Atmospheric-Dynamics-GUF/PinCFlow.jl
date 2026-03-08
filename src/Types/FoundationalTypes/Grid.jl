41ce7d7589399a62b0c70f20b4ba3982c18107d4
Felix Jochum <jochum@iau.uni-frankfurt.de>
2026-03-08 20:56:09 +0100

    Fixed the safety check in the computation of the stretched vertical grid.

--- src/Types/FoundationalTypes/Grid.jl
+++ src/Types/FoundationalTypes/Grid.jl
@@ -213,7 +213,7 @@ function Grid(namelists::Namelists, constants::Constants, domain::Domain)::Grid
             ztildes[k] = vertical_grid_stretching(level * lref) / lref
         end
     end
-    !issorted(ztildes; lt = <=) &&
+    @ivy !issorted(ztildes[k0:(k0 + z_size - 1)]; lt = <=) &&
         error("Error in Grid: Impossible vertical grid stretching!")
     @ivy for k in 2:(z_size + 2 * nbz)
         zs[k] = 0.5 * (ztildes[k] + ztildes[k - 1])
