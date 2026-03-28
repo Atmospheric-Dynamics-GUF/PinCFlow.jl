bd177c8302405c7934682ba4f2cfdd669d1f0881
Felix Jochum <jochum@iau.uni-frankfurt.de>
2026-03-28 12:00:33 +0100

    Made the precompilation script a shell script. It also loads the necessary cluster modules now.

--- examples/scripts/levante/precompile.jl
+++
@@ -1,6 +0,0 @@
-# Configure MPI and HDF5.
-run(`julia --project=examples -e 'using MPIPreferences; MPIPreferences.use_system_binary(; library_names=["/sw/spack-levante/intel-oneapi-mpi-2021.5.0-mrcss7/mpi/2021.5.0/lib/release/libmpi.so"])'`)
-run(`julia --project=examples -e 'using HDF5; HDF5.API.set_libraries!("/sw/spack-levante/hdf5-1.12.1-jmeuy3/lib/libhdf5.so", "/sw/spack-levante/hdf5-1.12.1-jmeuy3/lib/libhdf5_hl.so")'`)
-
-# Precompile.
-run(`julia --project=examples -e 'using Pkg; Pkg.precompile()'`)
