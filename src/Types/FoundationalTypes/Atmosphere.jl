08c50494ce849b9d9f947518e86ae85582fbe431
NikolausLabusch <147927940+NikolausLabusch@users.noreply.github.com>
2026-05-26 11:37:43 +0200

    Fixed typo in the documentation.

--- src/Types/FoundationalTypes/Atmosphere.jl
+++ src/Types/FoundationalTypes/Atmosphere.jl
@@ -134,7 +134,7 @@ The background fields are given by
     P \\left(z \\right) & =
     \\begin{cases}
         p_0 \\left( 1 - \\frac{\\kappa\\sigma z}{\\theta_0}\\right)^{\\frac{1}{\\gamma - 1}} & z \\leq z_{\\mathrm{TP}}\\;, \\\\
-        p_0 \\exp\\left[-\\frac{\\sigma(z-z_{\\mathrm{TP}})}{\\gamma T_{\\mathrm{TP}}}\\right] & z > z_{\\mathrm{TP}} \\;,
+        p_{\\mathrm{TP}} \\exp\\left[-\\frac{\\sigma(z-z_{\\mathrm{TP}})}{\\gamma T_{\\mathrm{TP}}}\\right] & z > z_{\\mathrm{TP}} \\;,
     \\end{cases} \\\\
     \\bar{\\theta}\\left(z\\right) & =
     \\begin{cases}
