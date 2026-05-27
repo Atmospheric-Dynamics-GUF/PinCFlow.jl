0d4132916a09572c82436ca9f2a4f873b1ba9a53
b382140 <knop@iau.uni-frankfurt.de>
2026-05-27 17:39:32 +0200

    minor improvements

--- src/Update/turbulent_diffusion!.jl
+++ src/Update/turbulent_diffusion!.jl
@@ -293,14 +293,10 @@ function turbulent_diffusion!(
 )
     (; momentum_coupling, entropy_coupling, tracer_coupling) =
         state.namelists.turbulence
-    (; uold, vold) = state.variables.backups
-    (; u, v) = state.variables.predictands
 
     check_tke!(state)
 
     if momentum_coupling
-        uold .= copy(u)
-        vold .= copy(v)
         turbulent_diffusion!(state, dt, U())
         turbulent_diffusion!(state, dt, V())
         turbulent_diffusion!(state, dt, W())
