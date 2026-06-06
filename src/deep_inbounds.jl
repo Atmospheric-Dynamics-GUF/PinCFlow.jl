4e2dd24c33bccb54d9134bf647e7861c344b9ec9
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2026-06-06 11:06:56 +0200

    Simplified `deep_inbounds` (tracking `LineNumberNode`s wasn't helpful).

--- src/deep_inbounds.jl
+++ src/deep_inbounds.jl
@@ -1,38 +1,19 @@
 function deep_inbounds end
 
-function deep_inbounds(
-    x::Any;
-    current_lnn::Union{Nothing, LineNumberNode} = nothing,
-)
+function deep_inbounds(x::Any)::Any
     if isa(x, Expr)
-        # Update the current LineNumberNode.
-        for arg in x.args
-            if isa(arg, LineNumberNode)
-                current_lnn = arg
-            end
-        end
-
         if x.head === :function ||
            x.head === :-> ||
            (x.head === :(=) && isa(x.args[1], Expr) && x.args[1].head === :call)
-            # If no LineNumberNode has been encountered, use that of this
-            # function.
-            lnn =
-                current_lnn !== nothing ? current_lnn :
-                LineNumberNode(@__LINE__, @__FILE__)
-
-            # Insert @inbounds.
             x.args[2] = Expr(
                 :macrocall,
                 Symbol("@inbounds"),
-                lnn,
-                deep_inbounds(x.args[2]; current_lnn),
+                LineNumberNode(@__LINE__, @__FILE__),
+                deep_inbounds(x.args[2]),
             )
-
-            return x
         else
             for (index, arg) in enumerate(x.args)
-                x.args[index] = deep_inbounds(arg; current_lnn)
+                x.args[index] = deep_inbounds(arg)
             end
         end
     end
