0c62f95a95c9b2f52e40156470e1483d383c3453
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2026-06-07 17:27:50 +0200

    Simplified `ivy` again. Now it only adds additional macros where needed.

--- src/ivy.jl
+++ src/ivy.jl
@@ -1,32 +1,47 @@
 function ivy end
 
-function ivy(x::Any)::Any
+function ivy(x::Any; root::Bool = true)::Any
     if isa(x, Expr)
-        head = string(x.head)
-        args = string.(x.args)
-
-        if head in ("=", ".=")
-            x.args[2] = ivy(x.args[2])
-        elseif endswith(head, r"[^!=]=")
-            assignment = replace(head, r"[^.=]" => "")
-            operation = head[1:(end - 1)]
-            x = Meta.parse(
-                "$(args[1]) $assignment $(args[1]) $operation ($(args[2]))",
-            )
-            x.args[2] = ivy(x.args[2])
-        elseif head == "ref"
-            for index in eachindex(x.args)
-                x.args[index] = ivy(x.args[index])
+        if x.head === :macrocall && length(x.args) > 2
+            for index in 3:length(x.args)
+                if isa(x.args[index], Expr)
+                    x.args[index] = quote
+                        @inbounds @views $(ivy(x.args[index]; root = false))
+                    end
+                end
             end
-            x = quote
-                @inbounds @views $x
+        elseif x.head === :module
+            x.args[3] = quote
+                @inbounds @views $(ivy(x.args[3]; root = false))
+            end
+        elseif x.head === :macro ||
+               x.head === :function ||
+               x.head === :-> ||
+               (
+                   x.head === :(=) &&
+                   isa(x.args[1], Expr) &&
+                   x.args[1].head === :call
+               ) ||
+               x.head === :let
+            x.args[2] = quote
+                @inbounds @views $(ivy(x.args[2]; root = false))
+            end
+        elseif x.head === :comprehension || x.head === :generator
+            x.args[1] = quote
+                @inbounds @views $(ivy(x.args[1]; root = false))
             end
         else
             for index in eachindex(x.args)
-                x.args[index] = ivy(x.args[index])
+                x.args[index] = ivy(x.args[index]; root = false)
             end
         end
     end
 
-    return x
+    if root
+        return quote
+            @inbounds @views $x
+        end
+    else
+        return x
+    end
 end
