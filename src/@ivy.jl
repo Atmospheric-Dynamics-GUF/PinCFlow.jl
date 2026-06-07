d60863ef53971d912199130cca1ddc1db6343cef
Felix Jochum <fjochum@iau.uni-frankfurt.de>
2026-06-07 21:16:15 +0200

    As it turns out, `@views` can pass other macros, so putting it in front of arguments of other macro calls is a bad idea. This has been corrected. Also macros cannot be applied to modules.

--- src/@ivy.jl
+++ src/@ivy.jl
@@ -5,8 +5,6 @@
 
 Return the expression `x` with `@inbounds` and `@views` applied to it and all its subexpressions.
 
-This macro places `@inbounds` and `@views` in front of expression arguments of any macro call. Care must therefore be taken when combining `@ivy` with other macros.
-
 # Arguments
 
   - `x`: Input expression.
--- src/ivy.jl
+++ src/ivy.jl
@@ -3,7 +3,7 @@
 ivy(x::Any; root::Bool = true)::Any
 ```
 
-If `x` is an expression, climb its AST and apply `@inbounds` and `@views` to the bodies of scope blocks and macro calls that either of them is not able to pass into, apply `@inbounds` and `@views` to `x` itself if `root == true`, and return it.
+If `x` is an expression, climb its AST and apply `@inbounds` and `@views` to the bodies of scope blocks that either of them is not able to pass into, apply `@inbounds` and `@views` to `x` itself if `root == true`, and return it.
 
 # Arguments
 
@@ -15,19 +15,7 @@ function ivy end
 
 function ivy(x::Any; root::Bool = true)::Any
     if isa(x, Expr)
-        if x.head === :macrocall && length(x.args) > 2
-            for index in 3:length(x.args)
-                if isa(x.args[index], Expr)
-                    x.args[index] = quote
-                        @inbounds @views $(ivy(x.args[index]; root = false))
-                    end
-                end
-            end
-        elseif x.head === :module
-            x.args[3] = quote
-                @inbounds @views $(ivy(x.args[3]; root = false))
-            end
-        elseif x.head === :macro ||
+        if x.head === :macro ||
                x.head === :function ||
                x.head === :-> ||
                (
