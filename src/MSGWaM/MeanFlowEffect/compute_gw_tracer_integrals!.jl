fe9a5e2715c244bb407b5b00878786ae36a3ba61
b382140 <knop@iau.uni-frankfurt.de>
2026-09-07 11:49:23 +0200

    Remove `leading_order_tracer_fluxes`

--- src/MSGWaM/MeanFlowEffect/compute_gw_tracer_integrals!.jl
+++ src/MSGWaM/MeanFlowEffect/compute_gw_tracer_integrals!.jl
@@ -67,7 +67,17 @@ The zonal, meridional, and vertical fluxes are given by
 \\begin{align*}
     \\bar{\\rho}\\left\\langle \\tilde{u} \\tilde{\\chi} \\right\\rangle & = \\frac{\\bar{\\rho}}{2} \\sum_{r, \\lambda,\\mu,\\nu} \\left[F \\Re \\left(u_\\mathrm{w}\\chi^*_\\mathrm{w}\\right)\\right]_{r, i + \\lambda, j + \\mu, k + \\nu},\\\\
     \\bar{\\rho}\\left\\langle \\tilde{v} \\tilde{\\chi} \\right\\rangle & = \\frac{\\bar{\\rho}}{2} \\sum_{r,  \\lambda, \\mu, \\nu} \\left[F \\Re \\left(v_\\mathrm{w} \\chi^*_\\mathrm{w}\\right)\\right]_{r, i + \\lambda, j + \\mu, k + \\nu},\\\\
-    \\bar{\\rho}\\left\\langle \\tilde{w} \\tilde{\\chi} \\right\\rangle & = \\frac{\\bar{\\rho}}{2} \\sum_{r, \\lambda, \\mu, \\nu} \\left[F \\Re \\left(w_\\mathrm{w} \\chi^*_\\mathrm{w}\\right)\\right]_{r, i + \\lambda, j + \\mu, k + \\nu}.
+    \\bar{\\rho}\\left\\langle \\tilde{w} \\tilde{\\chi} \\right\\rangle & = \\frac{\\bar{\\rho}}{2} \\sum_{r, \\lambda, \\mu, \\nu} \\left[F \\Re \\left(w_\\mathrm{w} \\chi^*_\\mathrm{w}\\right)\\right]_{r, i + \\lambda, j + \\mu, k + \\nu},
+\\end{align*}
+```
+
+with flux contributions given by 
+
+```math 
+\\begin{align*}
+\\frac{\\bar{\\rho}}{2} \\Re \\left(u_{\\mathrm{w}, r}\\chi^*_{\\mathrm{w}, r}\\right) & = \\frac{f}{\\hat{\\omega}_r} \\frac{m_r}{\\left|\\boldsymbol{k}_r\\right|^2} \\mathcal{A}_r \\left[l_r \\left(\\frac{\\partial \\chi_\\mathrm{b}}{\\partial z}\\right)_r - m_r \\left(\\frac{\\partial \\chi_\\mathrm{b}}{\\partial y}\\right)_r\\right], \\\\
+\\frac{\\bar{\\rho}}{2} \\Re \\left(v_{\\mathrm{w}, r} \\chi^*_{\\mathrm{w}, r}\\right) & = \\frac{f}{\\hat{\\omega}_{r}} \\frac{m_r}{\\left|\\boldsymbol{k}_{r}\\right|^2} \\mathcal{A}_r \\left[m_r \\left(\\frac{\\partial \\chi_\\mathrm{b}}{\\partial x}\\right)_r - k_r \\left(\\frac{\\partial \\chi_\\mathrm{b}}{\\partial z}\\right)_r\\right], \\\\
+\\frac{\\bar{\\rho}}{2} \\Re \\left(w_{\\mathrm{w}, r} \\chi^*_{\\mathrm{w}, r}\\right) & = \\frac{f}{\\hat{\\omega}_r} \\frac{m_r}{\\left|\\boldsymbol{k}_r\\right|^2} \\mathcal{A}_r \\left[k_r \\left(\\frac{\\partial \\chi_\\mathrm{b}}{\\partial y}\\right)_r - l_r \\left(\\frac{\\partial \\chi_\\mathrm{b}}{\\partial x}\\right)_r\\right].
 \\end{align*}
 ```
 
@@ -103,7 +113,7 @@ The zonal, meridional, and vertical fluxes are given by
 
 # See also
 
-  - [`PinCFlow.MSGWaM.MeanFlowEffect.leading_order_tracer_fluxes`](@ref)
+  - [`PinCFlow.MSGWaM.Interpolation.interpolate_mean_flow`](@ref)
 """
 function compute_gw_tracer_integrals! end
 
@@ -185,47 +195,15 @@ end
         return
     end
 
-    uchi0[i, j, k] += leading_order_tracer_fluxes(
-        state,
-        fc,
-        omir,
-        wnrk,
-        wnrl,
-        wnrm,
-        wadr,
-        xlc,
-        ylc,
-        zlc,
-        UChi(),
-    )
+    coeff = fc / omir * wnrm * wadr / (wnrk^2.0 + wnrl^2.0 + wnrm^2.0)
 
-    vchi0[i, j, k] += leading_order_tracer_fluxes(
-        state,
-        fc,
-        omir,
-        wnrk,
-        wnrl,
-        wnrm,
-        wadr,
-        xlc,
-        ylc,
-        zlc,
-        VChi(),
-    )
+    dchidx = interpolate_mean_flow(xlc, ylc, zlc, state, DChiDX())
+    dchidy = interpolate_mean_flow(xlc, ylc, zlc, state, DChiDY())
+    dchidz = interpolate_mean_flow(xlc, ylc, zlc, state, DChiDZ())
 
-    wchi0[i, j, k] += leading_order_tracer_fluxes(
-        state,
-        fc,
-        omir,
-        wnrk,
-        wnrl,
-        wnrm,
-        wadr,
-        xlc,
-        ylc,
-        zlc,
-        WChi(),
-    )
+    uchi0[i, j, k] += coeff * (wnrl * dchidz - wnrm * dchidy)
+    vchi0[i, j, k] += coeff * (wnrm * dchidx - wnrk * dchidz)
+    wchi0[i, j, k] += coeff * (wnrk * dchidy - wnrl * dchidx)
 
     return
 end
